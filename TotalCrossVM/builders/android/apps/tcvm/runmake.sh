2e26eaad0689d02a54555b418b3af0ac53767d13
fabio <fabio.sobral@superwaba.com.br>
2011-04-01 21:13:50 -0300

    Fixed tcvm android build, and moved temporary files to the temp folder. Still need to move the deployed libtcvm.

--- TotalCrossVM/builders/android/apps/tcvm/runmake.sh
+++ TotalCrossVM/builders/android/apps/tcvm/runmake.sh
@@ -1,2 +1,4 @@
-cd /cygdrive/p/TotalCrossVM/builders/android
+ln -s ${PWD}/TotalCross/temp/android/apps ${PWD}/TotalCross/TotalCrossVM/builders/android/out
+ln -s ${PWD}/TotalCross/temp/android/src ${PWD}/TotalCross/TotalCrossVM/builders/android/out
+cd ${PWD}/TotalCross/TotalCrossVM/builders/android
 make APP=tcvm -j $NUMBER_OF_PROCESSORS
