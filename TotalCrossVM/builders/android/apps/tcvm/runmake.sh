90201d402e1a46653cf069fbd91bf3e4bed14802
fabio <fabio.sobral@superwaba.com.br>
2011-04-04 15:45:32 -0300

    Updated path for android's runmake.

--- TotalCrossVM/builders/android/apps/tcvm/runmake.sh
+++ TotalCrossVM/builders/android/apps/tcvm/runmake.sh
@@ -1,4 +1,2 @@
-ln -s ${PWD}/TotalCross/temp/android/apps ${PWD}/TotalCross/TotalCrossVM/builders/android/out
-ln -s ${PWD}/TotalCross/temp/android/src ${PWD}/TotalCross/TotalCrossVM/builders/android/out
 cd ${PWD}/TotalCross/TotalCrossVM/builders/android
 make APP=tcvm -j $NUMBER_OF_PROCESSORS
