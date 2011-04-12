02708d7bac94a55d432ef63fb33589165fe4843c
fabio <fabio.sobral@superwaba.com.br>
2011-04-12 18:40:31 -0300

    Updated android build to use the android-ndk-r4b. The android ndk is no longer part of the TotalCrossVM project.

--- TotalCrossVM/builders/android/apps/tcvm/runmake.sh
+++ TotalCrossVM/builders/android/apps/tcvm/runmake.sh
@@ -1,2 +1,13 @@
-cd ${PWD}/TotalCross/TotalCrossVM/builders/android
+# Re-create all symbolic links
+rm -f ${PWD}/../android-ndk-r4b/apps
+rm -f ${PWD}/TotalCross/TotalCrossVM/builders/android/apps/tcvm/TotalCrossVM
+rm -f ${PWD}/../android-ndk-r4b/out
+rm -f ${PWD}/../android-ndk-r4b/TotalCrossVM
+ln -s ${PWD}/TotalCross/TotalCrossVM/builders/android/apps ${PWD}/../android-ndk-r4b
+ln -s ${PWD}/TotalCross/TotalCrossVM ${PWD}/TotalCross/TotalCrossVM/builders/android/apps/tcvm
+ln -s ${PWD}/TotalCross/TotalCrossVM/builders/android/out ${PWD}/../android-ndk-r4b
+ln -s ${PWD}/TotalCross/TotalCrossVM ${PWD}/../android-ndk-r4b
+
+# move to the ndk root folder
+cd ${PWD}/../android-ndk-r4b
 make APP=tcvm -j $NUMBER_OF_PROCESSORS
