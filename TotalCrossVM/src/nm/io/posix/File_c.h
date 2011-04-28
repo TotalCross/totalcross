fa4a1835f0234c761c8425b33d3e09b19b5a5d5d
guich <guich@superwaba.com.br>
2011-04-28 17:33:31 -0300

    added File.getSize(path) to get the free space

--- TotalCrossVM/src/nm/io/posix/File_c.h
+++ TotalCrossVM/src/nm/io/posix/File_c.h
@@ -248,7 +248,12 @@ static bool fileExists(TCHARP path, int32 slot)
 
 static inline Err fileGetFreeSpace(CharP szPath, int32* freeSpace, int32 slot)
 {
-#if defined(HAVE_STATFS)
+#if defined(ANDROID)
+   JNIEnv* env = getJNIEnv();
+   jmethodID method = (*env)->GetStaticMethodID(env, applicationClass, "fileGetFreeSpace", "(Ljava/lang/String;)I");
+   jstring fileName = (*env)->NewStringUTF(env, szPath);
+   *freeSpace = (*env)->CallStaticIntMethod(env, applicationClass, method, fileName);
+#elif defined(HAVE_STATFS)
    int64 fbytes = 0;
    struct statfs sfs;
    if (statfs(szPath, &sfs))
