f665f6416cfa549415ee60c4bfd9f4e67ae56244
fabio <fabio.sobral@superwaba.com.br>
2011-06-02 16:05:26 -0300

    Build palmposix to be used by the Litebase build.

--- LitebaseSDK/builders/gcc-palm/runmakeLB.sh
+++ LitebaseSDK/builders/gcc-palm/runmakeLB.sh
@@ -1,2 +1,5 @@
-cd ${PWD}/Litebase/LitebaseSDK/builders/gcc-palm
+CURRENT_DIR=${PWD}
+cd ${PWD}/TotalCross/TotalCrossVM/builders/gcc-palm/tcvm
+make_580 -f palmposix.mk
+cd ${CURRENT_DIR}/Litebase/LitebaseSDK/builders/gcc-palm
 make_580 $1 -j $NUMBER_OF_PROCESSORS
