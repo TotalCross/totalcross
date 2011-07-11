3da87c72b52903b09a92f346a8be5c3c26735c05
fabio <fabio.sobral@superwaba.com.br>
2011-07-11 11:50:18 -0300

    using relative paths on palm make to allow hudson to build from its workspace.

--- LitebaseSDK/builders/gcc-palm/runmakeLB.sh
+++ LitebaseSDK/builders/gcc-palm/runmakeLB.sh
@@ -1,5 +1,7 @@
 CURRENT_DIR=${PWD}
 cd ${PWD}/TotalCross/TotalCrossVM/builders/gcc-palm/tcvm
+PALM_BASE_DIR=${PWD}
+export PALM_BASE_DIR
 make_580 -f palmposix.mk
 cd ${CURRENT_DIR}/Litebase/LitebaseSDK/builders/gcc-palm
 make_580 $1 -j $NUMBER_OF_PROCESSORS
