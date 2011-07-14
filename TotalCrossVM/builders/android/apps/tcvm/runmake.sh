add359a4ff5ac71dae3c8c75fd5c1ff4591e5f5e
fabio <fabio.sobral@superwaba.com.br>
2011-07-14 16:05:26 -0300

    Fixed android build to work on hudson.

--- TotalCrossVM/builders/android/apps/tcvm/runmake.sh
+++ TotalCrossVM/builders/android/apps/tcvm/runmake.sh
@@ -1,13 +1,17 @@
+DROID_PROJECTS_HOME=$(cygpath -a ${DROID_PROJECTS_HOME})
+NDK_HOME=$(cygpath -a ${NDK_HOME})
+TC_HOME=$(cygpath -a ${TC_HOME})
+
 # Re-create all symbolic links
-rm -f ${PWD}/../android-ndk-r4b/apps
-rm -f ${PWD}/TotalCross/TotalCrossVM/builders/android/apps/tcvm/TotalCrossVM
-rm -f ${PWD}/../android-ndk-r4b/out
-rm -f ${PWD}/../android-ndk-r4b/TotalCrossVM
-ln -s ${PWD}/TotalCross/TotalCrossVM/builders/android/apps ${PWD}/../android-ndk-r4b
-ln -s ${PWD}/TotalCross/TotalCrossVM ${PWD}/TotalCross/TotalCrossVM/builders/android/apps/tcvm
-ln -s ${PWD}/TotalCross/TotalCrossVM/builders/android/out ${PWD}/../android-ndk-r4b
-ln -s ${PWD}/TotalCross/TotalCrossVM ${PWD}/../android-ndk-r4b
+rm -f ${NDK_HOME}/apps
+rm -f ${DROID_PROJECTS_HOME}/apps/tcvm/TotalCrossVM
+rm -f ${NDK_HOME}/out
+rm -f ${NDK_HOME}/TotalCrossVM
+ln -s ${DROID_PROJECTS_HOME}/apps ${NDK_HOME}
+ln -s ${TC_HOME} ${DROID_PROJECTS_HOME}/apps/tcvm
+ln -s ${DROID_PROJECTS_HOME}/out ${NDK_HOME}
+ln -s ${TC_HOME} ${NDK_HOME}
 
 # move to the ndk root folder
-cd ${PWD}/../android-ndk-r4b
+cd ${NDK_HOME}
 make APP=tcvm -j $NUMBER_OF_PROCESSORS
