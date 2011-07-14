de6d3dc56f7b4f8b021036863a19c4baa012cb11
guich <guich@superwaba.com.br>
2011-07-14 19:58:51 -0300

    added an easy way to compile the FixGCFragmentation for Prime Systems

--- TotalCrossVM/src/tcvm/objectmemorymanager.c
+++ TotalCrossVM/src/tcvm/objectmemorymanager.c
@@ -9,7 +9,7 @@
  *                                                                               *
  *********************************************************************************/
 
-
+//#define ALTERNATIVE_GC // used for Prime Systems on Palm OS devices
 
 #include "tcvm.h"
 
@@ -146,7 +146,11 @@ pointer to next):
 void soundTone(int32 frequency, int32 duration);
 
 #define MIN_SPACE_LEFT 16
-#define DEFAULT_CHUNK_SIZE (65536-96) // dlmalloc requires a bit more than 36 bytes. doing this to prevent allocating more than a 64k segment
+#ifdef ALTERNATIVE_GC
+ #define DEFAULT_CHUNK_SIZE (65536-96) // dlmalloc requires a bit more than 36 bytes. doing this to prevent allocating more than a 64k segment
+#else
+ #define DEFAULT_CHUNK_SIZE 65500
+#endif
 #define OBJARRAY_MAX_INDEX 128 // 4,8,12,16....4*OBJARRAY_MAX_INDEX
 
 inline static int32 size2idx(int32 size) // size must exclude sizeof(TObjectProperties) !
@@ -863,6 +867,16 @@ void runFinalizers() // calls finalize of all objects in use
    mainContext->litebasePtr = gcContext->litebasePtr; // update the ptr
 }
 
+#ifndef ALTERNATIVE_GC
+ #if defined(PALMOS) || defined(ANDROID) || defined(WINCE)
+  #define CRITICAL_SIZE 2*1024*1024
+  #define USE_MAX_BLOCK true
+ #else
+  #define CRITICAL_SIZE 5*1024*1024
+  #define USE_MAX_BLOCK false
+ #endif
+#endif
+
 void gc(Context currentContext)
 {
    int32 i;
@@ -879,7 +893,11 @@ void gc(Context currentContext)
       SystemIdleTimerReset();
 #endif
 #if !defined(ENABLE_TEST_SUITE) // this scrambles the test
-   if (IS_VMTWEAK_ON(VMTWEAK_DISABLE_GC) || elapsed < 500) // guich@tc114_18: let user control gc runs - guich@tc130: removed CRITICAL_TIME to fix memory fragmentation problems on 
+#ifdef ALTERNATIVE_GC
+   if ( IS_VMTWEAK_ON(VMTWEAK_DISABLE_GC) || elapsed < 500) // guich@tc114_18: let user control gc runs - guich@tc130: removed CRITICAL_TIME to fix memory fragmentation problems on 
+#else
+   if ((IS_VMTWEAK_ON(VMTWEAK_DISABLE_GC) || elapsed < 500) && getFreeMemory(USE_MAX_BLOCK) > CRITICAL_SIZE) // use an agressive gc if memory is under 2MB - guich@tc114_18: let user control gc runs
+#endif
    {
       skippedGC++;
       if (COMPUTETIME) debug("G ====  GC SKIPPED DUE %dms < 500ms", elapsed);
