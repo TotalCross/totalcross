2cb46c19d0e62edb495ed91a8341531ae07cd932
guich <guich@superwaba.com.br>
2011-07-26 12:10:15 -0300

    changed the ListContainerSample

--- TotalCrossSDK/src/tc/samples/ui/androidui/ListContainerSamples.java
+++ TotalCrossSDK/src/tc/samples/ui/androidui/ListContainerSamples.java
@@ -28,46 +28,32 @@ public class ListContainerSamples extends BaseContainer
          if (TOTAL_ITEMS > 1000)
             Flick.defaultLongestFlick = TOTAL_ITEMS * 3;
          
-         // "on" image
-         final Image off = new Image("totalcross/res/android/radioBkg.png");
-         // "off" image is a composite of two images: on + selection
-         final Image on = off.getFrameInstance(0);
-         final Image ball = new Image("totalcross/res/android/radioSel.png");
-         ball.applyColor2(Color.RED); // paint it
-         on.getGraphics().drawImage(ball,0,0,Graphics.DRAW_PAINT,Color.WHITE,true);
+         // "normal" image
+         final Image normal = new Image("images/plusButton.png");
+         
+         // pressed image: colorize the original one
+         final Image pressed = new Image("images/plusButton.png");
+         pressed.applyColor(Color.BLUE);
          
          ListContainer lc = new ListContainer();
          add(lc, LEFT,TOP,FILL,FILL);
          
          ListContainer.Layout layout = lc.getLayout(5,2);
          layout.insets.set(50,10,50,10);
-         layout.defaultLeftImage = off;
+         layout.defaultLeftImage = new Image("images/restaurant.png");
          layout.leftImageEnlargeIfSmaller = true;
-         layout.defaultRightImage = off;
-         layout.defaultRightImage2 = on;
+         layout.defaultRightImage = normal;
+         layout.defaultRightImage2 = pressed;
          layout.boldItems[1] = layout.boldItems[3] = true;
          layout.controlGap = 10; // 10% of font's height
          layout.defaultItemColors[1] = Color.RED;
          layout.lineGap = 25; // 1/4 font's height
          layout.relativeFontSizes[2] = layout.relativeFontSizes[3] = -1;
          layout.positions[3] = RIGHT;
-         layout.positions[4] = CENTER;
          layout.setup();
          
-         ListContainer.Item c = new ListContainer.Item(layout);
-         c.items = new String[]{"00011 ","BAR LANCHONETE CONRADO","Rio de Janeiro/Centro"," 99999,99","Brasil"};
-         lc.addContainer(c);
+         ListContainer.Item c;
          
-         c = new ListContainer.Item(layout);
-         c.items = new String[]{"00015 ","BARITMOS RESTAURANTE","Rio de Janeiro/Copacabana"," 80000,00","Também Brasil"};
-         c.leftControl = new Check(" "); ((Check)c.leftControl).checkColor = Color.RED;
-         lc.addContainer(c);
-         
-         c = new ListContainer.Item(layout);
-         c.items = new String[]{"00015 ","BARITMOS RESTAURANTE","Rio de Janeiro/Copacabana"," 80000,00","Também Brasil"};
-         c.leftControl = new Check(" "); ((Check)c.leftControl).checkColor = Color.RED;
-         lc.addContainer(c);
-
          Vm.gc();
          int gcTime = Settings.gcTime;
          int gcCount = Settings.gcCount;
@@ -76,8 +62,7 @@ public class ListContainerSamples extends BaseContainer
          for (int i = 0; i < all.length; i++)
          {
             all[i] = c = new ListContainer.Item(layout);
-            c.items = new String[]{Convert.zeroPad(i+1,5)," BARITONOS LANCHONETE","Rio de Janeiro/Leme","75000,00","Perú"};;
-            c.leftControl = null;  
+            c.items = new String[]{Convert.zeroPad(i+1,5)," BARITONOS LANCHONETE","Price","75000,00","Rio de Janeiro / Leme"};;
          }
          int ini2 = Vm.getTimeStamp();
          lc.addContainers(all);
@@ -85,6 +70,13 @@ public class ListContainerSamples extends BaseContainer
          gcTime = Settings.gcTime - gcTime;
          gcCount = Settings.gcCount - gcCount;
          setInfo("C="+(ini2-ini)+", A="+(ini3-ini2)+", T="+(ini3-ini)+", gc: "+gcTime+"/"+gcCount+"x"); 
+
+         /** Example just to show that other Controls can be set as a left and right control: 
+         c = new ListContainer.Item(layout);
+         c.items = new String[]{"00015 ","BARITMOS RESTAURANTE","Rio de Janeiro/Copacabana"," 80000,00","Também Brasil"};
+         c.leftControl = new Check(" "); ((Check)c.leftControl).checkColor = Color.RED;
+         lc.addContainer(c);
+         */
       }
       catch (Exception ee)
       {
