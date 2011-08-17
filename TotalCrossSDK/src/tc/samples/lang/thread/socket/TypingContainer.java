a9fb76c2b5b70c36d35b007c8938518c011581de
guich <guich@superwaba.com.br>
2011-08-17 12:24:45 -0300

    fixed support for screen rotation in ThreadedSocket

--- TotalCrossSDK/src/tc/samples/lang/thread/socket/TypingContainer.java
+++ TotalCrossSDK/src/tc/samples/lang/thread/socket/TypingContainer.java
@@ -22,7 +22,7 @@ import totalcross.sys.*;
 import totalcross.ui.*;
 import totalcross.ui.gfx.*;
 
-public class TypingContainer extends Container implements Runnable
+public class TypingContainer extends Container implements Runnable, ThreadedSocket.SetX
 {
    String typingText = "The new virtual machine, called TotalCross, has better performance due to a new instruction set that eliminates limitations in the existing SuperWaba virtual machine, with enhancements such as unlimited object size, preemptive threads and a new high-performance garbage collector that is 20X faster than the SuperWaba's. Additionally, deployed files are now compacted, to acheive a 30% reduction in size over SuperWaba applications.";
 
@@ -35,18 +35,29 @@ public class TypingContainer extends Container implements Runnable
       this.fill = fill;
    }
 
+   public void setX(int x)
+   {
+      this.x = x;
+   }
+   
+   public void incX(int x)
+   {
+      this.x += x;
+   }
+   
    public void initUI()
    {
       super.initUI();
-      setBackColor(Color.WHITE);
+      setBackColor(Color.brighter(fill ? Color.YELLOW : Color.GREEN));
       setBorderStyle(BORDER_RAISED);
 
       me = new MultiEdit(0,0);
-      add(me,0,0,width,height);
+      add(me,LEFT,TOP,FILL,FILL);
       if (fill)
          me.justify = fill;
       me.setEditable(false);
       me.hasCursorWhenNotEditable = false;
+      me.setBackColor(fill ? Color.YELLOW : Color.GREEN);
 
       Thread t = new Thread(this);
       t.start();
