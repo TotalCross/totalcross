84aa80d39bc588a0bef71b9b9415da10ccaf0b15
guich <guich@superwaba.com.br>
2011-08-29 17:45:11 -0300

    removed lines at top

--- TotalCrossSDK/src/tc/tools/deployer/Deployer4WinCE.java
+++ TotalCrossSDK/src/tc/tools/deployer/Deployer4WinCE.java
@@ -9,8 +9,6 @@
  *                                                                               *
  *********************************************************************************/
 
-
-
 package tc.tools.deployer;
 
 import totalcross.io.DataStream;
