5f7ef9bfaa63ff786775666475b5099dc7284dba
fabio <fabio.sobral@superwaba.com.br>
2011-09-28 13:23:34 -0300

    Removed the bug I introduced to test the jenkins error e-mail.

--- TotalCrossVM/src/nm/sys/palm/Vm_c.h
+++ TotalCrossVM/src/nm/sys/palm/Vm_c.h
@@ -52,7 +52,6 @@ static void vmSetTime(Object time) // (Lwaba/sys/Time;)V
 
 static void vmClipboardCopy(CharP str, int32 sLen) // (Ljava/lang/String;)V
 {
-   xpto
    ClipboardAddItem(clipboardText, str, sLen);
 }
 
