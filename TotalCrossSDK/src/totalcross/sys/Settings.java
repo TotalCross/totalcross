ea08be3ec4035477edcc6751be5190c69e8264f6
guich <guich@superwaba.com.br>
2011-11-08 17:20:11 -0200

    updated version to 1.37

--- TotalCrossSDK/src/totalcross/sys/Settings.java
+++ TotalCrossSDK/src/totalcross/sys/Settings.java
@@ -32,10 +32,10 @@ public final class Settings
     * version value of 400. A beta 0.8 VM will have version 80.
     * ps: Waba 1.0G will return 1.01. TotalCross = 110 (1.1) and beyond.
     */   // not declared final to prevent compile time optimizations!
-    public static int version = 136;
+    public static int version = 137;
     
     /** <b>READ-ONLY</b> variable that represents the version in a string form, like "1.36beta" */
-    public static String versionStr = "1.36";
+    public static String versionStr = "1.37";
 
    /** Can be one of the following constants: DATE_MDY, DATE_DMY, DATE_YMD; where m = month, d = day and y = year
     * @see #DATE_DMY
