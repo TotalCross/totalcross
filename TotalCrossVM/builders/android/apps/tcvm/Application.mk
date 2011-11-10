5d7e51836effd03a30db5f4a6852cb45e32e930b
guich <guich@superwaba.com.br>
2011-11-10 12:33:03 -0200

    5% speed improvement with these two switches

--- TotalCrossVM/builders/android/apps/tcvm/Application.mk
+++ TotalCrossVM/builders/android/apps/tcvm/Application.mk
@@ -2,4 +2,4 @@ APP_PROJECT_PATH := $(call my-dir)
 APP_MODULES      := tcvm
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
 APP_OPTIM        := release
-APP_CFLAGS       += -O3 -fprefetch-loop-arrays
+APP_CFLAGS       += -O3 -fpeel-loops -fprefetch-loop-arrays
