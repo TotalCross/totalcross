6b507869c9c200eeb78a93f8832c3e1986e0844f
guich <guich@superwaba.com.br>
2011-11-10 15:38:01 -0200

    faster way to convert to 16bpp

--- TotalCrossVM/src/nm/ui/android/gfx_ex.h
+++ TotalCrossVM/src/nm/ui/android/gfx_ex.h
@@ -16,7 +16,7 @@
 
 #define SETPIXEL32(r,g,b) (((r) << 16) | ((g) << 8) | (b))           // 00RRGGBB
 #define SETPIXEL565(r,g,b) ((((r) >> 3) << 11) | (((g) >> 2) << 5) | (((b) >> 3))) // bits RRRRRGGGGGGBBBBB
-#define SETPIXEL565_(dest,p) do {uint32 temp = p & 0xF800F800; *dest++ = ( (temp>>16) | ((p>>13)&0x7E0) | (temp>>11) );} while(0);
+#define SETPIXEL565_(dest,p) do {*dest++ = ((p>>16)&0xF800) | ((p>>13)&0x7E0) | ((p>>11)&0x1F );} while(0);
 
 typedef struct
 {
