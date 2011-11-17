d2c4799f2e2d6832d570720e05311cc13889f029
guich <guich@superwaba.com.br>
2011-11-17 10:54:46 -0200

    test how much alpha affects speed.

--- TotalCrossSDK/src/totalcross/ui/ProgressBar.java
+++ TotalCrossSDK/src/totalcross/ui/ProgressBar.java
@@ -216,6 +216,7 @@ public class ProgressBar extends Control
                int type = vertical ? width < fmH ? NinePatch.SCROLLPOSV : NinePatch.PROGRESSBARV : height < fmH ? NinePatch.SCROLLPOSH : NinePatch.PROGRESSBARH;
                npback = NinePatch.getInstance().getNormalInstance(type,width,height,bc,false,true);
                npfore = NinePatch.getInstance().getNormalInstance(type,width,height,fc,false,true);
+               npback.useAlpha = npfore.useAlpha = false;
             }
             
             if (endless) // only horizontal
