3db2944f18e8a051ed21ef105f1e55ab968a1edf
guich <guich@superwaba.com.br>
2011-11-21 13:00:14 -0200

    optimizing

--- TotalCrossVM/src/nm/ui/darwin/childview.m
+++ TotalCrossVM/src/nm/ui/darwin/childview.m
@@ -50,11 +50,11 @@ char* createPixelsBuffer(int width, int height);
       [self setWantsLayer:YES];
 
       screenLayer = [self layer];
-/*      [screenLayer setMagnificationFilter:0];
+      [screenLayer setMagnificationFilter:0];
       [screenLayer setEdgeAntialiasingMask:0];
       [screenLayer setFrame: CGRectMake(0, 0, width+1, height+1)];
       [screenLayer setOpaque:YES];
-      [[self layer] addSublayer:screenLayer];*/
+      //[[self layer] addSublayer:screenLayer];
    }  
    return self; 
 }
