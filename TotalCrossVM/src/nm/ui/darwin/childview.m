fff50a1c9c364651b89dd819bd9e32c35d08d375
guich <guich@superwaba.com.br>
2011-11-22 12:17:09 -0200

    test

--- TotalCrossVM/src/nm/ui/darwin/childview.m
+++ TotalCrossVM/src/nm/ui/darwin/childview.m
@@ -99,8 +99,8 @@ char* createPixelsBuffer(int width, int height);
    //debug("frame: %d %d %d %d",(int)frame.origin.x, (int)frame.origin.y, (int)frame.size.width, (int)frame.size.height);
    //CGContextClipToRect(bitmapContext, frame);
    cgImage = CGBitmapContextCreateImage(bitmapContext);
-   screenLayer.contentsRect = frame;
-   screenLayer.contents = (id)cgImage;
+   screenLayer.setContentsRect(frame);
+   screenLayer.setContents((id)cgImage);
    CGImageRelease(cgImage);
 }
 
