f897a68a61c5abd0c4742d745112690f25adc2d1
guich <guich@superwaba.com.br>
2011-11-23 14:53:01 -0200

    fixing iphone

--- TotalCrossVM/src/nm/ui/darwin/childview.m
+++ TotalCrossVM/src/nm/ui/darwin/childview.m
@@ -110,9 +110,11 @@ char* createPixelsBuffer(int width, int height);
          CGContextTranslateCTM(context, -height, 0);
          break;
       case kOrientationVerticalUpsideDown:
+         CGContextScaleCTM(context, 1, -1);
+         CGContextTranslateCTM(context, 0, height);
 //            CGContextScaleCTM(context, 1, -1);
-            CGContextTranslateCTM(context, width, 0);
-            CGContextRotateCTM(context, M_PI);
+//            CGContextTranslateCTM(context, width, 0);
+//            CGContextRotateCTM(context, M_PI);
          //CGContextTranslateCTM(context, width, height);
          //CGContextRotateCTM(context, -M_PI);
          break;
