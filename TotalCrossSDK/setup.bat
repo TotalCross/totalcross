c0ece0c4f65695b97eb4db7ef46ffcedb77f24aa
unknown <Fabio@Fabio-PC.(none)>
2011-12-22 20:01:27 -0200

    Win7 is very restrictive about creating environment variables, and errorlevel was not being correctly set so the variables were not being set for the user either.

--- TotalCrossSDK/setup.bat
+++ TotalCrossSDK/setup.bat
@@ -1,9 +1,11 @@
 @echo off
+SETLOCAL
 echo Setting up TOTALCROSS_HOME environment variable...
-call %CD%\etc\tools\setenv\SetEnv.exe -a TOTALCROSS_HOME %CD%
-if errorlevel 5 call %CD%\etc\tools\setenv\SetEnv.exe -ua TOTALCROSS_HOME %CD%
-call %CD%\etc\tools\setenv\SetEnv.exe -a PATH %"~TOTALCROSS_HOME~\dist\vm\win32"
-if errorlevel 5 call call %CD%\etc\tools\setenv\SetEnv.exe -ua PATH %"~TOTALCROSS_HOME~\dist\vm\win32"
+call %CD%\etc\tools\setenv\SetEnv.exe -a TOTALCROSS_HOME "%CD%"
+if errorlevel 1 call %CD%\etc\tools\setenv\SetEnv.exe -ua TOTALCROSS_HOME "%CD%"
+call %CD%\etc\tools\setenv\SetEnv.exe -a PATH %%"~TOTALCROSS_HOME~\dist\vm\win32"
+if errorlevel 1 call %CD%\etc\tools\setenv\SetEnv.exe -ua PATH %%"~TOTALCROSS_HOME~\dist\vm\win32"
+ENDLOCAL
 echo Creating Install Android registry key
 echo %CD%
 copy PostInstall.* %CD%\dist\vm\win32
