e7271a9d838de34b5fc7ed7a1d46ea45b5348b50
guich <guich@superwaba.com.br>
2011-12-30 16:32:37 -0200

    fixed AIOOBE on ComboBox

--- TotalCrossSDK/src/totalcross/ui/ComboBox.java
+++ TotalCrossSDK/src/totalcross/ui/ComboBox.java
@@ -536,7 +536,7 @@ public class ComboBox extends Container
       boolean trickW = pop.lb.width == 0; // guich@tc125_35
       if (trickW) pop.lb.width = width - btnW;
       int ih = pop.lb.itemCount > 0 ? pop.lb.getItemHeight(0) : fmH;
-      boolean isString = pop.lb.itemCount > 0 && pop.lb.items.items[pop.lb.selectedIndex] instanceof String;
+      boolean isString = pop.lb.itemCount > 0 && pop.lb.items.items[0] instanceof String;
       pop.lb.drawSelectedItem(g, pop.lb.selectedIndex, uiPalm ? btnW + 3 : 3, isString && uiAndroid ? (height-(fmH+Edit.prefH))/2 : (height - ih)/2); // guich@200b4: let the listbox draw the item
       if (trickW) pop.lb.width = 0;
    }
