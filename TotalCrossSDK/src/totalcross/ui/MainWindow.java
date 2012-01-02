0e733d2b8931353d4b73d993ecd022cbb908d04c
guich <guich@superwaba.com.br>
2012-01-02 14:32:31 -0200

    zero pad demo minutes

--- TotalCrossSDK/src/totalcross/ui/MainWindow.java
+++ TotalCrossSDK/src/totalcross/ui/MainWindow.java
@@ -456,7 +456,7 @@ public class MainWindow extends Window implements totalcross.MainClass
       DemoBox()
       {
          super(tit = "TotalCross Virtual Machine "+Settings.versionStr,
-               msg = "Copyright (c) 2008-2012\nSuperWaba Ltda\n\n"+(hasLitebase()?"TotalCross and Litebase\n":"")+"DEMO VERSION\n\nTime available: "+(timeAvailable == 0 ? "EXPIRED!" : (timeAvailable/100)+"h"+(timeAvailable%100)+"m"),
+               msg = "Copyright (c) 2008-2012\nSuperWaba Ltda\n\n"+(hasLitebase()?"TotalCross and Litebase\n":"")+"DEMO VERSION\n\nTime available: "+(timeAvailable == 0 ? "EXPIRED!" : (timeAvailable/100)+"h"+Convert.zeroPad(timeAvailable%100,2)+"m"),
                new String[]{"   Ok   "});
          Vm.debug(tit);
          Vm.debug(msg);
