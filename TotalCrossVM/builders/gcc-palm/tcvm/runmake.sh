907d72613205af8a86febce117227b265e4a54e9
unknown <Fabio@Fabio-Notebook.(none)>
2012-02-28 11:49:29 -0300

    Fixed NORAS build targets for PalmOS.

--- TotalCrossVM/builders/gcc-palm/tcvm/runmake.sh
+++ TotalCrossVM/builders/gcc-palm/tcvm/runmake.sh
@@ -1,3 +1,23 @@
+while [ $1 ]
+do
+	case "$1" in
+		-demo)
+		export TYPE=demo
+		shift
+		;;
+		-release)
+		export TYPE=release
+		shift
+		;;
+		-noras)
+		shift
+		export NORASID="$1"
+		export TYPE=noras
+		shift
+		;;
+	esac
+done
+
 PALM_BASE_DIR=$(cygpath $1)
 export PALM_BASE_DIR
 cd $PALM_BASE_DIR
