18527e27982fe880ba722e580b87d63cb63c62ae
fabio <fabio.sobral@superwaba.com.br>
2012-05-02 17:17:26 -0300

    Never inline functions on iOS.

--- LitebaseSDK/src/native/Table.c
+++ LitebaseSDK/src/native/Table.c
@@ -3260,7 +3260,7 @@ bool setModified(Context context, Table* table)
 }
 
 
-inline int32 randBetween(int32 low, int32 high)
+inline_ int32 randBetween(int32 low, int32 high)
 {
    TRACE("randBetween")
 
