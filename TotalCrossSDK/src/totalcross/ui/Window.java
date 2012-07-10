75e6d8fb541638065faaa80abdbb84a95ad09a84
guich <guich@superwaba.com.br>
2012-07-10 15:31:06 -0300

    this change to fix infinite loop on a pressed button was leading to AIOOBE when selecting a menu item and dragging to the menubar and releasing the button

--- TotalCrossSDK/src/totalcross/ui/Window.java
+++ TotalCrossSDK/src/totalcross/ui/Window.java
@@ -692,7 +692,7 @@ public class Window extends Container
             _penEvent.modifiers = modifiers;
             _penEvent.target = null;
             _penEvent.touch();
-            if (_focus != null && _focus == _dragEvent.target && type == PenEvent.PEN_UP) // guich@gc153: fixed problem of clicking in the Calendar's button making it repeat and dragging the mouse outside the window. without this, the button will repeat forever
+            if (_focus != null && _focus != this && _focus == _dragEvent.target && type == PenEvent.PEN_UP) // guich@gc153: fixed problem of clicking in the Calendar's button making it repeat and dragging the mouse outside the window. without this, the button will repeat forever
                _focus.postEvent(_penEvent);
             if (!onClickedOutside(_penEvent)) // if clicked outside was not handled by this method...
                if (type == PenEvent.PEN_DOWN && beepIfOut) // alert him! - ds: i changed this accordingly to your comments about win32 problems
