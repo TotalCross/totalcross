e054af04beb616d44dac03bef6423aa74ea47561
guich <guich@superwaba.com.br>
2012-07-31 16:39:38 -0300

    changed the way that the third server works. Now you create, besides the activation.html, other three files: activation_success.html (which is shown if the activation succeeds), activation_error.html (shown if the server rejects the activation), and activation_nointernet.html (a test is made before calling the webservice to make sure that there's internet, if there's not, this page is shown). This way you can customize all messages in your own language.

--- TotalCrossSDK/src/ras/ui/ActivationWindow.java
+++ TotalCrossSDK/src/ras/ui/ActivationWindow.java
@@ -14,9 +14,11 @@ package ras.ui;
 import ras.*;
 
 import totalcross.io.*;
+import totalcross.net.*;
 import totalcross.sys.*;
 import totalcross.ui.*;
 import totalcross.ui.dialog.*;
+import totalcross.ui.event.*;
 import totalcross.ui.font.*;
 import totalcross.ui.gfx.*;
 import totalcross.xml.soap.*;
@@ -41,73 +43,113 @@ public class ActivationWindow extends MainWindow
       super("", NO_BORDER);
       setUIStyle(Settings.Android);
       setBackColor(Color.WHITE);
-
       this.client = client;
    }
-
+   
    public void initUI()
    {
-      int c1 = 0x0A246A;
-      Font f = font.adjustedBy(2,true);
-      Bar headerBar = new Bar("Activation");
-      headerBar.createSpinner(Color.WHITE);
-      headerBar.setFont(f);
-      headerBar.setBackForeColors(c1,Color.WHITE);
-      add(headerBar, LEFT,0,FILL,PREFERRED);
-
-      html = ActivationHtml.getInstance();
+      if (Settings.keyboardFocusTraversable)
+         Settings.geographicalFocus = true;
+      html = ActivationHtml.getInstance(ActivationHtml.ACTIVATION_START);
       if (html != null)
-      {
-         if (Settings.keyboardFocusTraversable)
-            Settings.geographicalFocus = true;
-         int yy = headerBar.getY2();
-         html.setRect(0,yy,SCREENSIZE,SCREENSIZE+80);
-         html.popup();
-         html = null;
-      }
-
-      Label l = new Label("The TotalCross virtual machine needs to be activated. This process requires your device's internet connection to be properly set up.");
-      l.autoSplit = true;
-      l.align = FILL;
-      add(l,LEFT+5,AFTER+2,FILL-5,PREFERRED);
-
-      headerBar.startSpinner();
-      
-      repaintNow();
-
-      try
-      {
-         if (client == null)
-            Vm.sleep(3000);
-         else
-            client.activate();
-         headerBar.stopSpinner();
-         MessageBox mb = new MessageBox("Success", "TotalCross is now activated!\nPlease restart your application.");
-         mb.setBackColor(0x008800);
-         mb.titleColor = Color.WHITE;
-         mb.yPosition = BOTTOM;
-         mb.popup();
-         exit(0);
-      }
-      catch (ActivationException ex)
-      {
-         headerBar.stopSpinner();
-         Throwable cause = ex.getCause();
-         String s = ex.getMessage() + " The activation process cannot continue. The application will be terminated.";
-
-         if (cause instanceof SOAPException || cause instanceof IOException)
-            s += " Try again 2 or 3 times if there's really an internet connection.";
-
-         s = s.replace('\n', ' '); // guich@tc115_13
-         MessageBox mb = new MessageBox("Failure", s, new String[] { "  Exit  " });
-         mb.setTextAlignment(LEFT);
-         mb.titleColor = Color.WHITE;
-         mb.yPosition = BOTTOM;
-         mb.popup();
-         exit(1);
+         swap(html);
+      else
+      {      
+         int c1 = 0x0A246A;
+         Font f = font.adjustedBy(2,true);
+         Bar headerBar = new Bar("Activation");
+         headerBar.createSpinner(Color.WHITE);
+         headerBar.setFont(f);
+         headerBar.setBackForeColors(c1,Color.WHITE);
+         add(headerBar, LEFT,0,FILL,PREFERRED);
+   
+         Label l = new Label("The TotalCross virtual machine needs to be activated. This process requires your device's internet connection to be properly set up.");
+         l.autoSplit = true;
+         l.align = FILL;
+         add(l,LEFT+5,AFTER+2,FILL-5,PREFERRED);
+   
+         headerBar.startSpinner();
+         
+         repaintNow();
+   
+         try
+         {
+            if (client == null)
+               Vm.sleep(3000);
+            else
+               client.activate();
+            headerBar.stopSpinner();
+            success();
+         }
+         catch (ActivationException ex)
+         {
+            headerBar.stopSpinner();
+            failure(ex);
+         }
       }
    }
 
+   private void failure(ActivationException ex)
+   {
+      Throwable cause = ex.getCause();
+      String s = ex.getMessage() + " The activation process cannot continue. The application will be terminated.";
+
+      if (cause instanceof SOAPException || cause instanceof IOException)
+         s += " Try again 2 or 3 times if there's really an internet connection.";
+
+      alert("Failure",s,-1);
+      exit(1);
+   }
+
+   private void success()
+   {
+      alert("Success", "TotalCross is now activated!\nPlease restart your application.",0x008800);
+      exit(0);
+   }
+
+   public void onEvent(Event e)
+   {
+      if (e.type == ControlEvent.PRESSED && html != null && e.target == html)
+      {
+         if (html.type != ActivationHtml.ACTIVATION_START)
+            exit(html.type == ActivationHtml.ACTIVATION_SUCCESS ? 0 : 1);
+         else
+            try
+            {
+               if (!Socket.isInternetAccessible())
+                  throw new ActivationException("", new IOException());
+               if (client == null)
+                  Vm.sleep(3000);
+               else
+                  client.activate();
+               html = ActivationHtml.getInstance(ActivationHtml.ACTIVATION_SUCCESS);
+               if (html != null)
+                  swap(html);
+               else
+                  success();
+            }
+            catch (ActivationException ex)
+            {
+               Throwable cause = ex.getCause();
+               cause.printStackTrace();
+               html = ActivationHtml.getInstance(cause instanceof SOAPException || cause instanceof IOException ? ActivationHtml.ACTIVATION_NOINTERNET : ActivationHtml.ACTIVATION_ERROR);
+               if (html != null)
+                  swap(html);
+               else
+                  failure(ex);
+            }
+      }
+   }
+   
+   private void alert(String tit, String s, int bg)
+   {
+      MessageBox mb = new MessageBox(tit, s.replace('\n', ' '), new String[] { "  Exit  " });
+      mb.setTextAlignment(LEFT);
+      mb.titleColor = Color.WHITE;
+      mb.yPosition = BOTTOM;
+      mb.popup();
+   }
+   
    public void screenResized()
    {
       if (html != null && Settings.isLandscape())
