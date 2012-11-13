29777cf51346f408e5a33d11c93d7604521749d9
guich <guich@superwaba.com.br>
2012-11-13 16:37:28 -0200

    updated ios native methods table

--- TotalCrossVM/src/init/nativeProcAddressesTC.c
+++ TotalCrossVM/src/init/nativeProcAddressesTC.c
@@ -41,6 +41,7 @@ void fillNativeProcAddressesTC()
    htPutPtr(&htNativeProcAddresses, hashCode("tuiI_setTransparentColor_i"), &tuiI_setTransparentColor_i);
    htPutPtr(&htNativeProcAddresses, hashCode("tuiI_applyChanges"), &tuiI_applyChanges);
    htPutPtr(&htNativeProcAddresses, hashCode("tuiI_freeTexture"), &tuiI_freeTexture);
+   htPutPtr(&htNativeProcAddresses, hashCode("tugG_fillShadedRect_iiiibbiii"), &tugG_fillShadedRect_iiiibbiii);
    htPutPtr(&htNativeProcAddresses, hashCode("tugG_dither_iiii"), &tugG_dither_iiii);
    htPutPtr(&htNativeProcAddresses, hashCode("tugG_drawCylindricShade_iiiiii"), &tugG_drawCylindricShade_iiiiii);
    htPutPtr(&htNativeProcAddresses, hashCode("tugG_drawWindowBorder_iiiiiiiiii"), &tugG_drawWindowBorder_iiiiiiiiii);
@@ -82,7 +83,7 @@ void fillNativeProcAddressesTC()
    htPutPtr(&htNativeProcAddresses, hashCode("tugG_refresh_iiiiiif"), &tugG_refresh_iiiiiif);
    htPutPtr(&htNativeProcAddresses, hashCode("tugG_drawVistaRect_iiiiiiii"), &tugG_drawVistaRect_iiiiiiii);
    htPutPtr(&htNativeProcAddresses, hashCode("tugG_draw3dRect_iiiibbbI"), &tugG_draw3dRect_iiiibbbI);
-   htPutPtr(&htNativeProcAddresses, hashCode("tugG_fillVistaRect_iiiibbI"), &tugG_fillVistaRect_iiiibbI);
+   htPutPtr(&htNativeProcAddresses, hashCode("tugG_fillVistaRect_iiiiibb"), &tugG_fillVistaRect_iiiiibb);
    htPutPtr(&htNativeProcAddresses, hashCode("tugG_drawArrow_iiibbi"), &tugG_drawArrow_iiibbi);
    htPutPtr(&htNativeProcAddresses, hashCode("tugG_drawImage_iii"), &tugG_drawImage_iii);
    htPutPtr(&htNativeProcAddresses, hashCode("tugG_fillEllipseGradient_iiii"), &tugG_fillEllipseGradient_iiii);
