57d38c76a69c56b75ff7046c04c8d2c75f6d9663
guich <guich@superwaba.com.br>
2012-11-27 17:32:43 -0200

    fixed paths

--- TotalCrossVM/src/launchers/android/makelauncher_jar.bat
+++ TotalCrossVM/src/launchers/android/makelauncher_jar.bat
@@ -1,4 +1,4 @@
-cd bin\classes
+cd bin
 jar cvf Launcher.jar totalcross\app\stub\*.class totalcross\*.class
 move Launcher.jar P:\gitrepo\TotalCross\TotalCrossSDK\etc\launchers\android
 rem copy resources.ap_ P:\gitrepo\TotalCross\TotalCrossSDK\etc\launchers\android                                                   
