0b525a5027b86f0bd08e9275cd533fbc321117e9
guich <guich@superwaba.com.br>
2012-12-07 16:35:10 -0200

    fixed some cursor position erros and also infinite loop in MultiEdit

--- TotalCrossSDK/src/totalcross/ui/MultiEdit.java
+++ TotalCrossSDK/src/totalcross/ui/MultiEdit.java
@@ -470,7 +470,6 @@ public class MultiEdit extends Container implements Scrollable
    private void calculateFirst() // guich@320_28: completely redesigned - guich@581_6: highly optimized
    {
       StringBuffer chars = this.chars; // cache
-      int charsLen = chars.length();
       int i=0, originalLineCount = numberTextLines;
       first.removeAllElements();
       first.addElement(0); // in line 0, the first char is always 0
@@ -478,14 +477,8 @@ public class MultiEdit extends Container implements Scrollable
       int n = chars.length();
       for (int pos = 0; pos < n; pos++)
       {
-         if (pos == 0 || chars.charAt(pos-1) < ' ') // guich@tc113_37: when parsing "Update of /pcvsroot/LitebaseSDK/src/native/parser", it was breaking in the first /, but in the next loop iteration, it was skipping the first /, and, thus, computing a character less
-         {
-            first.addElement(pos = Convert.getBreakPos(fm, chars, pos, tw, true));
-            if (pos < charsLen && chars.charAt(pos) != ' ') // if last char is a space, don't show it at begining of next line
-               pos--;
-         }
-         else
-            first.addElement(pos = Convert.getBreakPos(fm, chars, pos-1, tw, true));
+         int pos0 = pos == 0 || chars.charAt(pos-1) < ' ' ? pos : pos-1; // guich@tc113_37: when parsing "Update of /pcvsroot/LitebaseSDK/src/native/parser", it was breaking in the first /, but in the next loop iteration, it was skipping the first /, and, thus, computing a character less
+         first.addElement(pos = Convert.getBreakPos(fm, chars, pos0, tw, true)); 
       }
       if (n == 0 || chars.charAt(n-1) == ENTER)
          first.addElement(n);
@@ -1221,6 +1214,8 @@ public class MultiEdit extends Container implements Scrollable
             if (!forceDrawAll) g.fillRect(boardRect.x + 1, h, boardRect.width - 2, hLine); // erase drawing area
             int k = first.items[i];
             int k2 = first.items[i + 1];
+            if (chars.charAt(k) <= ' ')
+               k++;
             g.drawText(chars, k, k2 - k, textRect.x, h, (!editable && justify && i < last && k2 < len && chars.charAt(k2) >= ' ') ? textRect.width : 0, textShadowColor != -1, textShadowColor); // don't justify if the line ends with <enter>
             if (drawDots)
             {
