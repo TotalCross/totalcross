a85e6e392bac0189280d5d2dc2bb187ea1d4bfc3
Juliana <juliana@superwaba.com.br>
2013-01-18 10:44:16 -0200

    Changed version.

--- LitebaseSDK/src/java/litebase/LitebaseConnection4D.java
+++ LitebaseSDK/src/java/litebase/LitebaseConnection4D.java
@@ -33,12 +33,12 @@ public class LitebaseConnection4D
    /**
     * The string corresponding to the current Litebase version.
     */
-   public static String versionStr = "2.66";
+   public static String versionStr = "2.67";
 
    /**
     * The integer corresponding to the current Litebase version.
     */
-   public static int version = 266;
+   public static int version = 267;
    
    /** 
     * Current build number.
