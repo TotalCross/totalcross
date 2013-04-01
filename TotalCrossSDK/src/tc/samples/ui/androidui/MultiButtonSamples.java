ca532990789931d0c7ca7f74c27ce9aa9f9471ed
guich <guich@superwaba.com.br>
2013-04-01 15:07:18 -0300

    sample for the new BORDER_ROUNDED style

--- TotalCrossSDK/src/tc/samples/ui/androidui/MultiButtonSamples.java
+++ TotalCrossSDK/src/tc/samples/ui/androidui/MultiButtonSamples.java
@@ -14,15 +14,17 @@ public class MultiButtonSamples extends BaseContainer
    {
       super.initUI();
       ScrollContainer sc = new ScrollContainer(false, true);
+      sc.borderColor = headerBar.getBackColor();
+      sc.setBorderStyle(BORDER_ROUNDED); // sample of the new rounded border
       sc.setInsets(gap, gap, gap, gap);
 
-      sc.add(new Label("Normal"),LEFT,TOP+fmH);
+      sc.add(new Label("Normal"),LEFT,TOP+fmH/2);
       MultiButton b = new MultiButton(new String[]{"+","-"});
       b.setBackColor(Color.ORANGE);
       b.is3dText = true;
       sc.add(b,SAME,AFTER,PREFERRED,fmH*3/2);
 
-      sc.add(new Label("Sticky with 3d text, center disabled"),LEFT,AFTER+fmH);
+      sc.add(new Label("Sticky, 3d text, center disabled"),LEFT,AFTER+fmH);
       b = new MultiButton(new String[]{"Left","Center","Right"});
       b.setBackColor(Color.GREEN);
       b.isSticky = b.is3dText = true;
@@ -30,7 +32,7 @@ public class MultiButtonSamples extends BaseContainer
       
       b.setEnabled(1,false);
 
-      add(sc, LEFT, TOP, FILL, FILL);
+      add(sc, LEFT+fmH, TOP+fmH, FILL-fmH, FILL-fmH);
       setInfo("Click Info button for help.");
    }
 }
