2c97f3a5f6778d00f1cc28b6be96a1f27c7542a2
Juliana <juliana@superwaba.com.br>
2013-07-01 15:29:07 -0300

    Updated version.

--- LitebaseSDK/src/java/litebase/LitebaseConnection.java
+++ LitebaseSDK/src/java/litebase/LitebaseConnection.java
@@ -34,7 +34,7 @@ public class LitebaseConnection
    /**
     * The string corresponding to the current Litebase version.
     */
-   public static String versionStr = "2.7";
+   public static String versionStr = "2.7TC2";
 
    /**
     * The integer corresponding to the current Litebase version.
