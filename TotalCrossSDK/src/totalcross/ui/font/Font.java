0cb19cc174843c8ee10ebcb3c691238608e44ef2
guich <guich@superwaba.com.br>
2013-09-16 17:50:41 -0300

    updated font size to 60

--- TotalCrossSDK/src/totalcross/ui/font/Font.java
+++ TotalCrossSDK/src/totalcross/ui/font/Font.java
@@ -64,7 +64,7 @@ public final class Font
    /** The minimum font size: 7. */
    public static int MIN_FONT_SIZE = 7;
    /** The maximum font size: 44 for Palm OS, 48 for other platforms. */
-   public static int MAX_FONT_SIZE = Settings.PALMOS.equals(Settings.platform) ? 44 : 48;
+   public static int MAX_FONT_SIZE = Settings.PALMOS.equals(Settings.platform) ? 44 : 60;
 
 
    /** Returns the default font size, based on the screen's size.
