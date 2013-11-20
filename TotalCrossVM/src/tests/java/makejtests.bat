570dc1763611ad78a45892f622104255fc25699e
Jefferson Quesado <jefferson.neto@totalcross.com>
2013-11-20 14:58:28 -0300

    Tests renewed! ANd also corrected some .bat bad behaviour

--- TotalCrossVM/src/tests/java/makejtests.bat
+++ TotalCrossVM/src/tests/java/makejtests.bat
@@ -1,7 +1,7 @@
 @echo off
 javac -target 1.1 -source 1.2 Test*.java
 jar cvf Test.jar Test*.class barbara.jpg pal685.png test.wav
-java.exe -classpath p:\gitrepo\TotalCross\TotalCrossSDK\output\eclipse;. tc.Deploy Test.jar -palm -win32 /a TCvm /d %1 >out.txt
+java.exe -classpath p:\gitrepo\TotalCross\TotalCrossSDK\output\eclipse;P:\gitrepo\TotalCross\TotalCrossVM\src\tests\java;p:\gitrepo\TotalCross\TotalCrossSDK\dist\tc.jar;. tc.Deploy Test.jar -win32 /a TCvm %1 >out.txt
 copy install\win32\Test.tcz P:\gitrepo\TotalCross\TotalCrossVM\builders\vc6\Debug
 rem copy Test.tcz Z:\TotalCross
 rem copy install\palm\Test.pdb Z:\TotalCross
