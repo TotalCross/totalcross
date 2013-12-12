26b00e20ce6d394dcb8e498d01c65c918a032e83
juliana <juliana@superwaba.com.br>
2013-12-12 17:51:33 -0300

    Oops!

--- TotalCrossVM/src/nm/sys/win/Vm_c.h
+++ TotalCrossVM/src/nm/sys/win/Vm_c.h
@@ -225,9 +225,10 @@ void vmSetAutoOff(bool enable)
 }
 
 //////////// START OF KEY INTERCEPTION FUNCTIONS
-
+//XXX: O que são hot keys? Não faço a menor idéia do que fazer aqui e acho que nem faz sentido
 void registerHotkeys(Int32Array keys, bool isRegister)
 {
+#ifndef WP8
    int32 n;
    if (mainHWnd != null)
    {
