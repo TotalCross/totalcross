9d714a7da02fa508ef80a5139c4b42563cf23908
Jefferson Quesado <jefferson.neto@totalcross.com>
2014-01-14 18:37:41 -0300

    Semantically, passing a null pointer to those functions is an excpetion more illegal argument than null pointer.
    Conflicts:
    	TotalCrossSDK/src/totalcross/io/ByteArrayStream.java

--- TotalCrossSDK/src/totalcross/io/ByteArrayStream.java
+++ TotalCrossSDK/src/totalcross/io/ByteArrayStream.java
@@ -39,7 +39,9 @@ public class ByteArrayStream extends RandomAccessStream
     */
    public ByteArrayStream(byte []buffer)
    {
-      this.len = buffer.length; //flsobral@tc100b4: fail-fast, throws NPE.
+      if (buffer == null)
+         throw new IllegalArgumentException("Argument 'buffer' cannot be null");
+      this.len = buffer.length;
       this.buffer = buffer;
       pos = 0;
    }
@@ -50,9 +52,11 @@ public class ByteArrayStream extends RandomAccessStream
    public ByteArrayStream(byte []buffer, int len)
    {
       if (buffer == null)
-         throw new NullPointerException("Argument 'buffer' cannot be null");
-      if (len <= 0)
-         throw new IllegalArgumentException("Argument 'len' must be greater than 0");
+         throw new IllegalArgumentException("Argument 'buffer' cannot be null");
+      if (len < 0)
+         throw new IllegalArgumentException("Argument 'len' must be greater or equal than 0");
+      if (len > buffer.length)
+          throw new IllegalArgumentException("Argument 'len' must not be greater than 'buffer.length'");
       this.len = len;
       this.buffer = buffer;
       pos = 0;
@@ -110,7 +114,9 @@ public class ByteArrayStream extends RandomAccessStream
     */
    public void setBuffer(byte[] buffer)
    {
-      len = buffer.length; //flsobral@tc100b4: fail-fast, throws NPE.
+      if (buffer == null)
+         throw new IllegalArgumentException("Argument 'buffer' cannot be null");
+      len = buffer.length;
       this.buffer = buffer;
       pos = 0;
    }
