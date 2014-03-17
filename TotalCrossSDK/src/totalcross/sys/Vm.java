c33751b3a7036b6f6702d869dae868e57c67f5dc
Guilherme Campos Hazan <guich@totalcross.com>
2014-03-17 16:53:45 -0300

    improved vibrate docs

--- TotalCrossSDK/src/totalcross/sys/Vm.java
+++ TotalCrossSDK/src/totalcross/sys/Vm.java
@@ -344,7 +344,8 @@ public final class Vm
       }
    }
    
-   /** Vibrates the device for the specified number of milliseconds.
+   /** Vibrates the device for the specified number of milliseconds. 
+    * The millis is ignored in iOS, since there's no way to define the duration in that platform.
     * Note that on WP8 the maximum vibration time is 5 seconds. If millis is greater than 5000, it will vibrate only for 5 seconds.
     * 
     * @since TotalCross 1.22
