ebcd14e60f1f0d817013fb4b6fe533d5eb079917
Guilherme Campos Hazan <guich@totalcross.com>
2014-05-21 14:55:40 -0300

    improved ui

--- TotalCrossSDK/src/tc/samples/api/net/SocketSample.java
+++ TotalCrossSDK/src/tc/samples/api/net/SocketSample.java
@@ -44,7 +44,7 @@ public class SocketSample extends BaseContainer
       add(edP = new Edit("8080"), AFTER+3, SAME);
       edP.setText("80");
       
-      add(btnOpen = new Button("Open connection"), LEFT, AFTER+3,PREFERRED,PREFERRED+fmH/4);
+      add(btnOpen = new Button(" Open connection "), CENTER, AFTER+3,PREFERRED,PREFERRED+fmH/4);
       
       addLog(LEFT,AFTER+3,FILL,FILL,null);
    }
