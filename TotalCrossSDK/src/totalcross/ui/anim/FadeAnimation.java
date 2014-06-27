ed3ec99788b8090fbeab2eb9410b6323fdfc1587
Guilherme Campos Hazan <guich@totalcross.com>
2014-06-27 15:32:21 -0300

    fixed fade speed

--- TotalCrossSDK/src/totalcross/ui/anim/FadeAnimation.java
+++ TotalCrossSDK/src/totalcross/ui/anim/FadeAnimation.java
@@ -5,14 +5,14 @@ import totalcross.ui.event.*;
 
 public class FadeAnimation extends ControlAnimation implements TimerListener
 {
-   int a,af;
+   int a,at,af;
    boolean fadeIn;
-   int speed;
    
    public FadeAnimation(Control c, boolean fadeIn, AnimationFinished animFinish)
    {
       super(c,animFinish);
       this.fadeIn = fadeIn;
+      at = 255;
       a = fadeIn ? 0 : 255;
       af = fadeIn ? 255 : 0;
    }
@@ -26,14 +26,13 @@ public class FadeAnimation extends ControlAnimation implements TimerListener
    {
       super.start();
       c.offscreen.alphaMask = a;
-      speed = (int)computeSpeed(255);
-      if (!fadeIn)
-         speed = -speed;
    }
    
    public void animate()
    {
-      a += speed;
+      int speed = (int)computeSpeed(at);
+      at -= speed;
+      a += fadeIn ? speed : -speed;
       if (a > 255) a = 255; else if (a < 0) a = 0;
       if (c.offscreen != null)
          c.offscreen.alphaMask = a;
