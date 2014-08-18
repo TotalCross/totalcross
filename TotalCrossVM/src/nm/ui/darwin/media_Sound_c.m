09c7a0687ac543b265840fde1d3f23c64e2ce652
tcbuilder <fabio@superwaba.com.br>
2014-08-18 15:17:54 -0300

    implemented Sound.play in ios

--- TotalCrossVM/src/nm/ui/darwin/media_Sound_c.m
+++ TotalCrossVM/src/nm/ui/darwin/media_Sound_c.m
@@ -9,8 +9,19 @@
 #import <AudioToolbox/AudioToolbox.h>
 #include "xtypes.h"
 
+static SystemSoundID sound;
+static char lastSound[MAX_PATHNAME];
 void iphone_soundPlay(CharP filename)
 {
+   if (!strEq(filename,lastSound))
+   {
+      if (sound) AudioServicesDisposeSystemSoundID(sound);
+      xstrcpy(lastSound, filename);
+      NSString* string = [NSString stringWithFormat:@"%s", filename];
+      NSURL *wav = [NSURL fileURLWithPath:string];
+      AudioServicesCreateSystemSoundID((__bridge CFURLRef)wav, &sound);
+   }
+   AudioServicesPlaySystemSound(sound);
 }
 
 
