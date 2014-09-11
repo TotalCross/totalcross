bd6c7c6bd545285c7cfeee5d55709ebe528f8b4f
Guilherme Campos Hazan <guich@totalcross.com>
2014-09-11 15:10:29 -0300

    dont visit applications when we're finishing the application to prevent a crash (that occurs at least in android)

--- TotalCrossVM/src/tcvm/objectmemorymanager.c
+++ TotalCrossVM/src/tcvm/objectmemorymanager.c
@@ -892,7 +892,9 @@ void visitImages(VisitElementFunc onImage, int32 param) // visits all images
 {
    TCObjectArray usedL;
    TCObject o;
-   int32 i;
+   int32 i;                    
+   if (destroyingApplication) return;
+      
    for (i = 0, usedL = usedList; i <= OBJARRAY_MAX_INDEX; i++, usedL++)
       if (*usedL)
          for (o=OBJ_PROPERTIES(*usedL)->next; o != null; o = OBJ_PROPERTIES(o)->next)
