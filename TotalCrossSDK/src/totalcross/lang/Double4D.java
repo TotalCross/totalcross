01b6633dfd8d81bea3d4bfad2a506dc19bc2e4b8
juliana <juliana@superwaba.com.br>
2014-10-17 16:33:55 -0300

    Added some methods. The native implementations are not done yet.

--- TotalCrossSDK/src/totalcross/lang/Double4D.java
+++ TotalCrossSDK/src/totalcross/lang/Double4D.java
@@ -80,4 +80,76 @@ public class Double4D
    {
       return v == Convert.DOUBLE_NAN_VALUE;
    }
+   
+   /**
+    * Convert the double to the IEEE 754 floating-point "double format" bit
+    * layout. Bit 63 (the most significant) is the sign bit, bits 62-52
+    * (masked by 0x7ff0000000000000L) represent the exponent, and bits 51-0
+    * (masked by 0x000fffffffffffffL) are the mantissa. This function
+    * collapses all versions of NaN to 0x7ff8000000000000L. The result of this
+    * function can be used as the argument to
+    * <code>Double.longBitsToDouble(long)</code> to obtain the original
+    * <code>double</code> value.
+    *
+    * @param value the <code>double</code> to convert
+    * @return the bits of the <code>double</code>
+    * @see #longBitsToDouble(long)
+    */
+   public static long doubleToLongBits(double value)
+   {
+     if (isNaN(value))
+       return 0x7ff8000000000000L;
+     else
+       return doubleToRawLongBits(value);
+   }
+   
+   /**
+    * Convert the double to the IEEE 754 floating-point "double format" bit
+    * layout. Bit 63 (the most significant) is the sign bit, bits 62-52
+    * (masked by 0x7ff0000000000000L) represent the exponent, and bits 51-0
+    * (masked by 0x000fffffffffffffL) are the mantissa. This function
+    * leaves NaN alone, rather than collapsing to a canonical value. The
+    * result of this function can be used as the argument to
+    * <code>Double.longBitsToDouble(long)</code> to obtain the original
+    * <code>double</code> value.
+    *
+    * @param value the <code>double</code> to convert
+    * @return the bits of the <code>double</code>
+    * @see #longBitsToDouble(long)
+    */
+   public static native long doubleToRawLongBits(double value);
+
+   /**
+    * Behaves like <code>new Double(x).compareTo(new Double(y))</code>; in
+    * other words this compares two doubles, special casing NaN and zero,
+    * without the overhead of objects.
+    *
+    * @param x the first double to compare
+    * @param y the second double to compare
+    * @return the comparison
+    * @since 1.4
+    */
+   public static int compare(double x, double y)
+   {
+       // handle the easy cases:
+       if (x < y)
+           return -1;
+       if (x > y)
+           return 1;
+
+       // handle equality respecting that 0.0 != -0.0 (hence not using x == y):
+       long lx = doubleToRawLongBits(x);
+       long ly = doubleToRawLongBits(y);
+       if (lx == ly)
+           return 0;
+
+       // handle NaNs:
+       if (x != x)
+           return (y != y) ? 0 : 1;
+       else if (y != y)
+           return -1;
+
+       // handle +/- 0.0
+       return (lx < ly) ? -1 : 1;
+   }
 }
