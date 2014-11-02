97d34b5cc80a9b3f8ab20250de10b0a1308b47d5
Guilherme Campos Hazan <guich@superwaba.com.br>
2014-11-02 09:00:42 -0300

    prevent program crash on dlmalloc failure

--- TotalCrossVM/src/util/dlmalloc.c
+++ TotalCrossVM/src/util/dlmalloc.c
@@ -500,6 +500,8 @@ MAX_RELEASE_CHECK_RATE   default: 4095 unless not HAVE_MMAP
 #define FOOTERS 1
 #define DEFAULT_GRANULARITY 2*1024*1024 // // guich@tc124_17: 2MB
 #define DEFAULT_MMAP_THRESHOLD MAX_SIZE_T // // guich@tc124_17: DISABLE
+#define ABORT_ON_ASSERT_FAILURE 0 // prevent program
+#define PROCEED_ON_ERROR 0        //  crash
 #endif
 
 void debug(char *s, ...);
@@ -513,7 +515,6 @@ void debug(char *s, ...);
 
 //////// TOTALCROSS CHANGES FINISH /////////
 
-#define ABORT_ON_ASSERT_FAILURE 0
 #ifndef WIN32
 #ifdef _WIN32
 #define WIN32 1
