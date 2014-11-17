df4abbb2d03fb9adb362af90ca332c653bcbfc94
Guilherme Campos Hazan <guich@totalcross.com>
2014-11-17 16:03:38 -0300

    fixed problem when using synchronized and an exception is thrown.

--- TotalCrossSDK/src/tc/tools/converter/Bytecode2TCCode.java
+++ TotalCrossSDK/src/tc/tools/converter/Bytecode2TCCode.java
@@ -1507,14 +1507,34 @@ public class Bytecode2TCCode implements JConstants, TCConstants
          }
          case ATHROW: //191
          {
-            if (!stack.empty())
-            {
-               OperandReg regO = (OperandReg) stack.pop();
-               Reg code = new Reg(THROW, lineOfPC);
-               code.set(regO.index);
-               vcode.addElement(code);
-               stack.push(regO);
-            }
+            /*
+             With this code:
+             
+               Lock objectLock = new Lock();
+               String connection = "";
+               
+               public String recoverTable() {
+                  synchronized (objectLock) {
+                      return connection;
+                  }
+               }
+             
+             Eclipse has an optimization that removes an extra move of the lock from the local
+             variables to the stack. This is the code it generates:
+             
+             14: aload_1
+             15: monitorexit
+             16: athrow
+             
+             At line 14, the stack holds the exception at top, and the lock at local variable 1.
+             However, the TC converter isn't aware that the exception is at the top of the stack,
+             so we check it and provide that extra operand that tells it.
+             */
+            OperandReg regO = stack.empty() ? new OperandRegO() : (OperandReg) stack.pop();
+            Reg code = new Reg(THROW, lineOfPC);
+            code.set(regO.index);
+            vcode.addElement(code);
+            stack.push(regO);
             break;
          }
          case JCHECKCAST: //192
