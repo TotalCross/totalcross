a31c5b5f1512e4874a377fe81f6ab0afdd137b97
Guilherme Campos Hazan <guich@superwaba.com.br>
2015-04-16 00:01:43 -0300

    empty string date means null

--- TotalCrossSDK/src/totalcross/sql/sqlite4j/SQLConvert.java
+++ TotalCrossSDK/src/totalcross/sql/sqlite4j/SQLConvert.java
@@ -25,7 +25,7 @@ class SQLConvert
    {
       try
       {
-         return x == null ? null : new Time(x, true,true,true,true,true,true, Settings.DATE_YMD);
+         return x == null || x.isEmpty() ? null : new Time(x, true,true,true,true,true,true, Settings.DATE_YMD);
       }
       catch (Exception e)
       {
