ae73864c4d307977055d0c47093aa1b82957aa14
Guilherme Campos Hazan <guich@superwaba.com.br>
2015-05-21 19:13:07 -0300

    improving sample

--- TotalCrossSDK/src/tc/samples/like/fb/FBImages.java
+++ TotalCrossSDK/src/tc/samples/like/fb/FBImages.java
@@ -8,14 +8,17 @@ public class FBImages
    
    public static void load(int fmH) throws Exception
    {
-      chat    = new Image("img/chat.png").smoothScaledFixedAspectRatio(fmH,true);
-      content = new Image("img/content.png").smoothScaledFixedAspectRatio(fmH,true);
-      friends = new Image("img/friends.png").smoothScaledFixedAspectRatio(fmH,true);
+      int top = fmH*3/2;
       group   = new Image("img/group.png").smoothScaledFixedAspectRatio(fmH,true);
-      menu    = new Image("img/menu.png").smoothScaledFixedAspectRatio(fmH,true);
       message = new Image("img/message.png").smoothScaledFixedAspectRatio(fmH,true);
-      news    = new Image("img/news.png").smoothScaledFixedAspectRatio(fmH,true);
-      online  = new Image("img/online.png").smoothScaledFixedAspectRatio(fmH,true);
-      search  = new Image("img/search.png").smoothScaledFixedAspectRatio(fmH,true);
+
+      content = new Image("img/content.png").smoothScaledFixedAspectRatio(top,true);
+      friends = new Image("img/friends.png").smoothScaledFixedAspectRatio(top,true);
+      chat    = new Image("img/chat.png").smoothScaledFixedAspectRatio(top,true);
+      news    = new Image("img/news.png").smoothScaledFixedAspectRatio(top,true);
+      menu    = new Image("img/menu.png").smoothScaledFixedAspectRatio(top,true);
+      
+      online  = new Image("img/online.png").smoothScaledFixedAspectRatio(top,true);
+      search  = new Image("img/search.png").smoothScaledFixedAspectRatio(top,true);
    }
 }
--- TotalCrossSDK/src/tc/samples/like/fb/FaceBookUI.java
+++ TotalCrossSDK/src/tc/samples/like/fb/FaceBookUI.java
@@ -10,14 +10,58 @@ public class FaceBookUI extends MainWindow
 {
    private static final int TOPBAR = 0x3C5998;
    private static final int BOTBAR = 0x47639E;
+
+   class MyEdit extends Edit
+   {
+      public void onPaint(Graphics g)
+      {
+         super.onPaint(g);
+         if (!hasFocus && getLength() == 0)
+         {
+            g.foreColor = 0xAAAAAA;
+            g.drawText("Busca",0,fmH/4);
+         }
+      }
+   }
    
    class TopBar extends Container
    {
+      Button btSearch, btOnline;
+      
       public void initUI()
       {
          setBackColor(TOPBAR);
-         add(noborder(FBImages.search), LEFT+100,CENTER);
-         add(noborder(FBImages.online), RIGHT-100,CENTER);
+         add(btSearch = noborder(FBImages.search), LEFT+100,CENTER);
+         add(btOnline = noborder(FBImages.online), RIGHT-100,CENTER);
+         Edit ed = new MyEdit();
+         ed.setForeColor(Color.WHITE);
+         ed.transparentBackground = true;
+         add(ed, AFTER,SAME,FIT,PREFERRED,btSearch);
+         Ruler r = new Ruler();
+         r.dots = true;
+         add(r, SAME,AFTER,SAME-100,1);
+      }
+   }
+   
+   class TopMenu extends Container
+   {
+      private Button create(Image img)
+      {
+         Button b = noborder(img);
+         try {(b.pressedImage = img.getCopy()).applyColor2(TOPBAR);} catch (Exception e) {}
+         b.shiftOnPress = false;
+         b.isSticky = true;
+         return b;
+      }
+      
+      public void initUI()
+      {
+         setBackColor(Color.WHITE);
+         add(create(FBImages.content),LEFT,CENTER,PARENTSIZE+20,PREFERRED);
+         add(create(FBImages.friends),AFTER,CENTER,PARENTSIZE+20,PREFERRED);
+         add(create(FBImages.chat),AFTER,CENTER,PARENTSIZE+20,PREFERRED);
+         add(create(FBImages.news),AFTER,CENTER,PARENTSIZE+20,PREFERRED);
+         add(create(FBImages.menu),AFTER,CENTER,PARENTSIZE+20,PREFERRED);
       }
    }
    
@@ -31,7 +75,7 @@ public class FaceBookUI extends MainWindow
    public FaceBookUI()
    {
       Settings.uiAdjustmentsBasedOnFontHeight = true;
-      setBackColor(Color.WHITE);
+      setUIStyle(Settings.Android);
    }
 
    public void initUI()
@@ -39,12 +83,39 @@ public class FaceBookUI extends MainWindow
       try
       {
          FBImages.load(fmH);
-         add(new TopBar(), LEFT,TOP,FILL,fmH*2);
+         add(new TopBar(), LEFT,TOP,FILL,fmH*5/2);
+         add(new TopMenu(), LEFT,AFTER,FILL,SAME);
+         add(h = new ContentHolder(),LEFT,AFTER,FILL,FILL); 
       }
       catch (Exception ee)
       {
          MessageBox.showException(ee,true);
          exit(0);
+      }
+   }
+   
+   ContentHolder h;
+   
+   class Content extends Container
+   {
+      public void initUI()
+      {
+         setBackColor(Color.WHITE);
+      }
+   }
+   
+   class ContentHolder extends ScrollContainer
+   {
+      public ContentHolder()
+      {
+         super(false,true);
+         setBackColor(0xDCDEE3);         
+      }
+      
+      public void initUI()
+      {
+         setInsets(fmH/2,fmH/2,2,2);
+         add(new Content(),LEFT,AFTER,FILL,PARENTSIZE+40);
       }
    }
 }
