1e9b0866b028e89b41b58bb9eeaafa6481cd226f
tcbuilder <fabio@superwaba.com.br>
2015-06-11 11:23:34 -0300

    fixed GPS and Camera no longer working on iOS 8

--- TotalCrossVM/src/nm/ui/darwin/mainview.m
+++ TotalCrossVM/src/nm/ui/darwin/mainview.m
@@ -340,6 +340,8 @@ static bool callingCamera;
          locationManager = [[CLLocationManager alloc] init];
          locationManager.delegate = self;
          locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+         if ([locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)])
+            [locationManager requestWhenInUseAuthorization];
          [locationManager startUpdatingLocation];
          locationCount = 0;
       });
