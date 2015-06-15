229fc3348771dbc88729618f894712da3bb20bb0
Guilherme Campos Hazan <guich@superwaba.com.br>
2015-06-15 17:27:55 -0300

    fixed negative array size

--- TotalCrossSDK/src/totalcross/ui/NinePatch.java
+++ TotalCrossSDK/src/totalcross/ui/NinePatch.java
@@ -62,9 +62,33 @@ public class NinePatch
    private NinePatch()
    {
       if (Settings.uiStyle == Settings.Holo)
-         parts = partsHolo;
+         parts = new Parts []
+         {
+            load(Resources.button,7,4), 
+            load(Resources.edit,16,2), 
+            load(Resources.combobox,5,2),
+            load(Resources.listbox,5,3),
+            load(Resources.multiedit,9,4), 
+            load(Resources.progressbarv,9,4),
+            load(Resources.scrollposh,3,2),
+            load(Resources.scrollposv,3,2),
+            load(Resources.tab,10,4),
+            load(Resources.grid,5,3),
+         };
       else
-         parts = partsAndroid;
+         parts = new Parts [] 
+         {
+            load(Resources.button,7,1), 
+            load(Resources.edit,5,3), 
+            load(Resources.combobox,5,2),
+            load(Resources.listbox,5,3),
+            load(Resources.multiedit,9,4), 
+            load(Resources.progressbarv,9,4),
+            load(Resources.scrollposh,3,2),
+            load(Resources.scrollposv,3,2),
+            load(Resources.tab,10,4),
+            load(Resources.grid,5,3),
+         };
    }
    
    public class Parts
@@ -76,34 +100,6 @@ public class NinePatch
    private Lock imageLock = new Lock();
    
    private Parts [] parts;
-   
-   private Parts [] partsAndroid = 
-   {
-      load(Resources.button,7,1), 
-      load(Resources.edit,5,3), 
-      load(Resources.combobox,5,2),
-      load(Resources.listbox,5,3),
-      load(Resources.multiedit,9,4), 
-      load(Resources.progressbarv,9,4),
-      load(Resources.scrollposh,3,2),
-      load(Resources.scrollposv,3,2),
-      load(Resources.tab,10,4),
-      load(Resources.grid,5,3),
-   };
-   
-   private Parts [] partsHolo = 
-   {
-      load(Resources.button,7,4), 
-      load(Resources.edit,16,2), 
-      load(Resources.combobox,5,2),
-      load(Resources.listbox,5,3),
-      load(Resources.multiedit,9,4), 
-      load(Resources.progressbarv,9,4),
-      load(Resources.scrollposh,3,2),
-      load(Resources.scrollposv,3,2),
-      load(Resources.tab,10,4),
-      load(Resources.grid,5,3),
-   };
    
    private Hashtable htBtn = new Hashtable(100); 
    private Hashtable htPressBtn = new Hashtable(100); 
