114536e51533caab5e520bd681ca0aea26fcd88b
Guilherme Campos Hazan <guich@totalcross.com>
2015-08-21 16:47:42 -0300

    finished implementation of the new Switch component. Added sample in TotalCrossAPI

--- TotalCrossSDK/src/totalcross/ui/Switch.java
+++ TotalCrossSDK/src/totalcross/ui/Switch.java
@@ -8,16 +8,27 @@ import totalcross.ui.event.*;
 import totalcross.ui.gfx.*;
 import totalcross.ui.image.*;
 
-public class Switch extends Control implements AnimationFinished
+/** This class implements the on/off switch present in many platforms
+ * 
+ */
+public class Switch extends Control implements PathAnimation.SetPosition, AnimationFinished
 {
-   Image back,backLR;
-   Image btn;
-   boolean isIos;
-   int startDragPos, dragBarPos, dragBarSize, dragBarMin, dragBarMax;
+   private Image back,backLR;
+   private Image btn;
+   private boolean isIos,dragged,wasChecked;
+   private int startDragPos, dragBarPos, dragBarSize, dragBarMin, dragBarMax;
+   /** The animation time. */
+   public static int ANIMATION_TIME = 250;
+   
+   /** Text to draw when on background or foreground, and when this switch is on or off */
+   public String textBackOn, textForeOn, textBackOff, textForeOff;
+   
+   /** Text color to draw when on background or foreground, and when this switch is on or off */
+   public int colorBackOn, colorForeOn, colorBackOff, colorForeOff;
    
    public Switch(boolean androidType)
    {
-      foreColor = Color.CYAN;
+      foreColor = 0x05B6EE;
       backColor = 0xDDDDDD;
       this.isIos = !androidType;
    }
@@ -29,9 +40,23 @@ public class Switch extends Control implements AnimationFinished
       dragBarMin = 0;
       dragBarMax = width - dragBarSize-1;
    }
+   
    public void onColorsChanged(boolean b)
    {
       back = btn = null;
+   }
+   
+   /** Change the on/off state. */
+   public void setOn(boolean b)
+   {
+      if (b != isOn())
+         moveSwitch(!b);
+   }
+   
+   /** Returns true if this switch is ON */
+   public boolean isOn()
+   {
+      return dragBarPos+dragBarSize/2 >= width/2;
    }
    
    public void moveSwitch(boolean toLeft)
@@ -40,16 +65,9 @@ public class Switch extends Control implements AnimationFinished
       if (dragBarPos != destPos)
          try
          {
-            PathAnimation p = PathAnimation.create(this, dragBarPos,0, destPos, 0, this, 300);
+            PathAnimation p = PathAnimation.create(this, dragBarPos,0, destPos, 0, this, ANIMATION_TIME);
             p.useOffscreen = false;
-            p.setpos = new PathAnimation.SetPosition() 
-            {
-               public void setPos(int x, int y)
-               {
-                  dragBarPos = x;
-                  Window.needsPaint = true;
-               }
-            };
+            p.setpos = this;
             p.start();
          }
          catch (Exception ee)
@@ -57,6 +75,13 @@ public class Switch extends Control implements AnimationFinished
             if (Settings.onJavaSE) ee.printStackTrace();
             dragBarPos = destPos;
          }
+   }
+   
+   /** Used by animation */
+   public void setPos(int x, int y) // PathAnimation.SetPosition
+   {
+      dragBarPos = x;
+      Window.needsPaint = true;
    }
    
    public void onPaint(Graphics g)
@@ -95,16 +120,42 @@ public class Switch extends Control implements AnimationFinished
             g.drawImage(backLR,width-height,0);
             g.clearClip();
          }               
+         if (isOn())
+         {
+            if (textBackOn != null)
+            {
+               g.foreColor = colorBackOn;
+               g.drawText(textBackOn,(width-fm.stringWidth(textBackOn))/2,(height-fmH)/2);
+            }
+            g.drawImage(btn,dragBarPos,0);
+            if (textForeOn != null)
+            {
+               g.foreColor = colorForeOn;
+               g.drawText(textForeOn,dragBarPos+(dragBarSize-fm.stringWidth(textForeOn))/2,(height-fmH)/2);
+            }
+         }
+         else
+         {
+            if (textBackOff != null)
+            {
+               g.foreColor = colorBackOff;
+               g.drawText(textBackOff,(width-fm.stringWidth(textBackOff))/2,(height-fmH)/2);
+            }
+            g.drawImage(btn,dragBarPos,0);
+            if (textForeOff != null)
+            {
+               g.foreColor = colorForeOff;
+               g.drawText(textForeOff,dragBarPos+(dragBarSize-fm.stringWidth(textForeOff))/2,(height-fmH)/2);
+            }
+         }
       }
       catch (Throwable t)
       {
          t.printStackTrace();
       }
       // draw button
-      g.drawImage(btn,dragBarPos,0);
    }
    
-   private boolean dragged;
    public void onEvent(Event event)
    {
       switch (event.type)
@@ -125,16 +176,25 @@ public class Switch extends Control implements AnimationFinished
             }
             break;
          case PenEvent.PEN_DOWN:
+            wasChecked = isOn();
             dragged = false;
-            startDragPos = getPos(event);
+            startDragPos = ((PenEvent)event).x - dragBarPos;
             break;
          case PenEvent.PEN_UP:
             Window.needsPaint = true;
-            if (dragged && !hadParentScrolled())
+            if (!hadParentScrolled())
             {
-               dragBarPos = getPos(event);
-               boolean nowAtMidLeft = dragBarPos+dragBarSize/2 < width/2;
-               moveSwitch(nowAtMidLeft);
+               if (!dragged)
+               {
+                  boolean nowAtMidLeft = ((PenEvent)event).x < width/2;
+                  moveSwitch(nowAtMidLeft);
+               }
+               else
+               {
+                  dragBarPos = getPos(event);
+                  boolean nowAtMidLeft = dragBarPos+dragBarSize/2 < width/2;
+                  moveSwitch(nowAtMidLeft);
+               }
             }
             startDragPos = -1;
             break;
@@ -156,7 +216,7 @@ public class Switch extends Control implements AnimationFinished
 
    public void onAnimationFinished(ControlAnimation anim)
    {
-      // TODO Auto-generated method stub
-      
+      if (isOn() != wasChecked)
+         postPressedEvent();
    }
 }
