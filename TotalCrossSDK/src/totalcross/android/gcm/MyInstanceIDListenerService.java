03f5927304e452bd6b94371f0cb3dd2620bff2dc
Guilherme Campos Hazan <guich@superwaba.com.br>
2016-04-26 14:24:45 -0300

    code reformat

--- TotalCrossSDK/src/totalcross/android/gcm/MyInstanceIDListenerService.java
+++ TotalCrossSDK/src/totalcross/android/gcm/MyInstanceIDListenerService.java
@@ -3,19 +3,18 @@ package totalcross.android.gcm;
 import android.content.*;
 import com.google.android.gms.iid.*;
 import android.util.Log;
-public class MyInstanceIDListenerService extends InstanceIDListenerService 
+
+public class MyInstanceIDListenerService extends InstanceIDListenerService
 {
-    /**
-     * Called if InstanceID token is updated. This may occur if the security of
-     * the previous token had been compromised. This call is initiated by the
-     * InstanceID provider.
-     */
+   /**
+    * Called if InstanceID token is updated. This may occur if the security of the previous token had been compromised.
+    * This call is initiated by the InstanceID provider.
+    */
 
-    public void onTokenRefresh() 
-     {
-        // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
-       Log.i("TotalCross", "MyInstanceIDListenerService.onTokenRefresh");
-        startService(new Intent(this, GCMTokenReceiver.class));
-
-    }
+   public void onTokenRefresh()
+   {
+      // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
+      Log.i("TotalCross", "MyInstanceIDListenerService.onTokenRefresh");
+      startService(new Intent(this, GCMTokenReceiver.class));
+   }
 }
