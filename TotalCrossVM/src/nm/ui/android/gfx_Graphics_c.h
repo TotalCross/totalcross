7e8cf057f401061afb3d22f6bad0e601ce32100c
Guilherme <Guilherme@192.168.25.9>
2016-07-05 17:16:56 -0300

    clear previous error on opengl to prevent false positives

--- TotalCrossVM/src/nm/ui/android/gfx_Graphics_c.h
+++ TotalCrossVM/src/nm/ui/android/gfx_Graphics_c.h
@@ -780,6 +780,7 @@ static void setProjectionMatrix(float w, float h)
       0.0,      0.0,   0.0,  1.0
    };
 
+   checkGlError(null,-1); // clear previous error
    setCurrentProgram(textProgram);         glUniformMatrix4fv(textProjMat        , 1, 0, mat); GL_CHECK_ERROR
    setCurrentProgram(textureProgram);      glUniformMatrix4fv(textureProjMat     , 1, 0, mat); GL_CHECK_ERROR
    setCurrentProgram(lrpProgram);          glUniformMatrix4fv(lrpProjMat         , 1, 0, mat); GL_CHECK_ERROR
