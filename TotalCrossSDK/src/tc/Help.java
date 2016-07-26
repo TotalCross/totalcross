09228bccc6454f2c35d5d4866c5e06eaead3e617
Guilherme <Guilherme@192.168.25.4>
2016-07-26 20:59:41 -0300

    added a logo

--- TotalCrossSDK/src/tc/Help.java
+++ TotalCrossSDK/src/tc/Help.java
@@ -4,6 +4,7 @@ import totalcross.sys.*;
 import totalcross.ui.*;
 import totalcross.ui.event.*;
 import totalcross.ui.gfx.*;
+import totalcross.ui.image.*;
 
 public class Help extends MainWindow
 {
@@ -28,30 +29,39 @@ public class Help extends MainWindow
    
    public void initUI()
    {
-      removeAll();
-      RadioGroupController rg = new RadioGroupController();
-      add(new Label(x("Helper application to Run/Deploy", "Aplicação de ajuda para Executar / Empacotar"),CENTER,Color.BLUE,true),LEFT,TOP,FILL,PREFERRED);
-      add(new Label(x("Language: ","Linguagem: ")), LEFT,AFTER+25);
-      add(rEn = new Radio("English", rg), AFTER+25, SAME);    tip(rEn,"Click here to set the user interface to English","Clique aqui para mudar a linguagem para inglês");  
-      add(rPt = new Radio("Português", rg), AFTER+25, SAME);  tip(rPt,"Click here to set the user interface to Portuguese","Clique aqui para mudar a linguagem para português");
-      rg.setSelectedIndex(isEn ? 0 : 1,false);
-      add(new Label(x("Class name: ","Nome da classe: ")),LEFT,AFTER+25);
-      add(edclass = new Edit(),AFTER,SAME); tip(edclass, "Type the full class name of the class that extends MainWindow. Don't forget to include the package.", "Digite o nome (com o pacote) da classe que estende MainWindow"); 
-      add(new Label(x("Key: ","Chave: ")),LEFT,AFTER+25);
-      add(edkey = new Edit(),AFTER,SAME);  tip(edkey, "Type the registration key that you received by email", "Digite a chave que você recebeu por email");
-      
-      lstatus = new Label("",CENTER);
-      lstatus.setBackForeColors(COLOR,0);
-      lstatus.setFont(font.asBold());
-      lstatus.autoSplit = true;
-      add(lstatus,LEFT,BOTTOM,FILL,fmH*2);
-      
-      tc = new TabbedContainer(new String[]{x("Run","Executar"),x("Deploy","Empacotar")});
-      tc.setBackColor(COLOR);
-      tc.allSameWidth = true;
-      add(tc, LEFT,AFTER+25,FILL,FIT,edkey);
-      tc.setContainer(0,new RunContainer());
-      tc.setContainer(1,new DepContainer());
+      try
+      {
+         removeAll();
+         RadioGroupController rg = new RadioGroupController();
+         add(new ImageControl(new Image("logoh.png")),CENTER,TOP);
+         add(new Label(x("Helper application to Run/Deploy", "Aplicação de ajuda para Executar / Empacotar"),CENTER,Color.BLUE,true),LEFT,AFTER+50,FILL,PREFERRED);
+         add(new Label(x("Language: ","Linguagem: ")), LEFT,AFTER+25);
+         add(rEn = new Radio("English", rg), AFTER+25, SAME);    tip(rEn,"Click here to set the user interface to English","Clique aqui para mudar a linguagem para inglês");  
+         add(rPt = new Radio("Português", rg), AFTER+25, SAME);  tip(rPt,"Click here to set the user interface to Portuguese","Clique aqui para mudar a linguagem para português");
+         rg.setSelectedIndex(isEn ? 0 : 1,false);
+         add(new Label(x("Class name: ","Nome da classe: ")),LEFT,AFTER+25);
+         add(edclass = new Edit(),AFTER,SAME); tip(edclass, "Type the full class name of the class that extends MainWindow. Don't forget to include the package.", "Digite o nome (com o pacote) da classe que estende MainWindow"); 
+         add(new Label(x("Key: ","Chave: ")),LEFT,AFTER+25);
+         add(edkey = new Edit(),AFTER,SAME);  tip(edkey, "Type the 24-characters registration key that you received by email", "Digite a chave com 24 caracteres que você recebeu por email");
+         
+         lstatus = new Label("",CENTER);
+         lstatus.setBackForeColors(COLOR,0);
+         lstatus.setFont(font.asBold());
+         lstatus.autoSplit = true;
+         add(lstatus,LEFT,BOTTOM,FILL,fmH*2);
+         
+         tc = new TabbedContainer(new String[]{x("Run","Executar"),x("Deploy","Empacotar")});
+         tc.setBackColor(COLOR);
+         tc.allSameWidth = true;
+         add(tc, LEFT,AFTER+25,FILL,FIT,edkey);
+         tc.setContainer(0,new RunContainer());
+         tc.setContainer(1,new DepContainer());
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+         exit(1);
+      }
    }
    
    private void tip(Control c, String en, String pt)
@@ -87,6 +97,7 @@ public class Help extends MainWindow
       Switch swSc;
       Radio rdA, rdI, rd32, rdCE;
       Check chM;
+      Button btRun;
       
       public RunContainer()
       {
@@ -102,18 +113,18 @@ public class Help extends MainWindow
          add(edY = new Edit("99999"),AFTER,SAME); tip(edY, "Type the Y position for the window. Leave blank to let the system set it", "Digite a posição Y para a janela. Deixe em branco pro sistema posicionar");
          
          add(new Label(x("Select one to populate the edits below", "Selecione um para popular os edits abaixo")),LEFT,AFTER+25);
-         add(rd32 = new Radio("Win32",rg),LEFT,AFTER);     tip(rd32, "Width=240, height=320, bpp=24", "Largura=240, altura=320, bpp=24");
-         add(rdCE = new Radio("WinCE",rg),AFTER+50,SAME);  tip(rdCE, "Width=240, height=320, bpp=8", "Largura=240, altura=320, bpp=8");
-         add(rdA = new Radio("Android",rg),AFTER+50,SAME); tip(rdA,  "Width=320, height=480, bpp=24", "Largura=320, altura=480, bpp=24");
-         add(rdI = new Radio("iOS",rg),AFTER+50,SAME);     tip(rdI,  "Width=640, height=960, bpp=24, scale=0.75", "Largura=640, altura=960, bpp=24, escala=0.75");
+         add(rd32 = new Radio("Win32",rg),LEFT,AFTER);     rd32.appId = 1; tip(rd32, "Width=240, height=320, bpp=24", "Largura=240, altura=320, bpp=24");
+         add(rdCE = new Radio("WinCE",rg),AFTER+50,SAME);  rdCE.appId = 2; tip(rdCE, "Width=240, height=320, bpp=8", "Largura=240, altura=320, bpp=8");
+         add(rdA = new Radio("Android",rg),AFTER+50,SAME); rdA .appId = 3; tip(rdA,  "Width=320, height=480, bpp=24", "Largura=320, altura=480, bpp=24");
+         add(rdI = new Radio("iOS",rg),AFTER+50,SAME);     rdI .appId = 4; tip(rdI,  "Width=640, height=960, bpp=24, scale=0.75", "Largura=640, altura=960, bpp=24, escala=0.75");
          add(new Label(x("Width: ","Largura: ")),LEFT,AFTER+25);
          add(edW = new Edit("99999"),AFTER,SAME);   tip(edW, "Type the width for the window", "Digite a largura da janela");
          add(new Label(x("Height: ","Altura: ")),AFTER+50,SAME); 
          add(edH = new Edit("99999"),AFTER,SAME);   tip(edH, "Type the height for the window", "Digite a altura da janela");
          add(new Label(x("BitsPerPixel: ","BitsPorPixel: ")),LEFT,AFTER+25);
-         add(cbBpp = new ComboBox(new String[]{"8","16","32"}),AFTER,SAME); tip(cbBpp, "Select the bits per pixel to be used in the Window. All platforms uses 24bpp, except WinCE, which uses 8bpp", "Selecione os bits por pixel usado na janela. Todas as plataformas, menos WinCE, usam 24bpp");
+         add(cbBpp = new ComboBox(new String[]{"8","16","24","32"}),AFTER,SAME); tip(cbBpp, "Select the bits per pixel to be used in the Window. All platforms uses 24bpp, except WinCE, which uses 8bpp", "Selecione os bits por pixel usado na janela. Todas as plataformas, menos WinCE, usam 24bpp");
          add(new Label(x("Font size: ","Tam da letra: ")),LEFT,AFTER+25);
-         add(edFS = new Edit("99999"),AFTER,SAME);  tip(edFS, "Type the font size, which is the most important attribute after the window's size", "Digite o tamanho da fonte, que é o atributo mais importante após o tamanho da janela");
+         add(edFS = new Edit("99999"),AFTER,SAME);  tip(edFS, "Type the font size. Leave it blank to use the default one", "Digite o tamanho da fonte. Deixe vazio pra usar o tamanho padrão");
          add(new Label(x("Scale: ","Escala: ")),LEFT,AFTER+25);
          add(cbSc = new ComboBox(new String[]{"0.25","0.5","0.75","1","2","4","6","8"}),AFTER,SAME); tip(cbSc, "Select the scale to apply in the Window. Useful when you test screen sizes above your monitor's resolution", "Selecione a escala pra aplicar na janela. Útil quando usar tamanho de janela maior que a resolução do seu monitor");
          cbSc.setSelectedItem("1");
@@ -127,6 +138,41 @@ public class Help extends MainWindow
          add(new Label(x("Command line to pass to application: ","Linha de comandos para passar à aplicação")),LEFT,AFTER+25);
          add(edCmd = new Edit(),LEFT,AFTER);   tip(edCmd, "You can pass extra arguments to application and retrieve it using MainWindow.getCommandLine()", "Você pode passar argumentos extras pra aplicação e recuperá-los usando MainWindow.getCommandLine()");
          add(chM = new Check(x("Show mouse position","Mostrar posição do mouse")),LEFT,AFTER+25); tip(chM, "Show the mouse position on window's title area", "Mostra a posição do mouse no título da janela");
+         btRun = new Button("Run the application");
+         btRun.setBackColor(COLOR);
+         add(btRun, CENTER,AFTER+100,PARENTSIZE+80,PREFERRED+50);
+      }
+      
+      public void onEvent(Event e)
+      {
+         switch (e.type)
+         {
+            case ControlEvent.PRESSED:
+               if (e.target instanceof Radio)
+               {
+                  int w,h,bpp=24;
+                  String s="1";
+                  switch (((Control)e.target).appId)
+                  {
+                     case 2: // wince
+                        bpp = 8;
+                     case 1: // win32
+                        w = 240; h = 320; break;
+                     case 3: // android
+                        w = 320; h = 480; break;
+                     case 4: // ios
+                        w = 640; h = 960; s="0.75"; break;
+                     default:
+                        return;                        
+                  }
+                  edW.setText(String.valueOf(w));
+                  edH.setText(String.valueOf(h));
+                  cbBpp.setSelectedItem(String.valueOf(bpp));
+                  cbSc.setSelectedItem(s);
+               }
+                  
+               break;
+         }
       }
    }
    
