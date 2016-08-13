a6d5efc4b1512fcc4f9826675b9790f74429a54c
Guilherme <Guilherme@192.168.25.3>
2016-08-13 16:38:21 -0300

    trying to fix linux build

--- TotalCrossVM/src/minizip/crypt.h
+++ TotalCrossVM/src/minizip/crypt.h
@@ -27,7 +27,6 @@
    Encryption is not supported.
 */
 
-#include "../zlib/zconf.h"
 #define CRC32(c, b) ((*(pcrc_32_tab+(((int)(c) ^ (b)) & 0xff))) ^ ((c) >> 8))
 
 /***********************************************************************
