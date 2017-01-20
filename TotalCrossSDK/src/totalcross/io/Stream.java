1a207013b1aff03d2fc83f9fc814b83454908cdf
Jefferson Quesado <jeff.quesado@gmail.com>
2017-01-19 21:30:39 -0300

    Extraindo a classe por sei lá

--- TotalCrossSDK/src/totalcross/io/Stream.java
+++ TotalCrossSDK/src/totalcross/io/Stream.java
@@ -18,7 +18,6 @@
 
 package totalcross.io;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 import totalcross.sys.*;
@@ -149,32 +148,4 @@ public abstract class Stream extends Connection
    public InputStream wrapInputStream() {
 	   return new WrapInputStream(this);
    }
-}
-
-class WrapInputStream extends InputStream {
-	private Stream stream;
-
-	WrapInputStream(Stream stream) {
-		this.stream = stream;
-	}
-
-	@Override
-	public int read() throws IOException {
-		byte[] b = new byte[1];
-		if (read(b) == -1) {
-			return -1;
-		}
-		
-		return b[0];
-	}
-	
-	@Override
-	public int read(byte b[], int off, int len) throws IOException {
-		try {
-			return stream.readBytes(b, off, len);
-		} catch (totalcross.io.IOException e) {
-			throw new java.io.IOException(e);
-		}
-	}
-	
 }
