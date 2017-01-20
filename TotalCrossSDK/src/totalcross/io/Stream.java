c6e148721b326060a2b315204f16cf633edbb734
Jefferson Quesado <jeff.quesado@gmail.com>
2017-01-20 09:48:17 -0300

    Revert "Extraindo a classe por sei lá"
    This reverts commit 001d5cd4519b3513a5acbeb766255b91b307dea9.

--- TotalCrossSDK/src/totalcross/io/Stream.java
+++ TotalCrossSDK/src/totalcross/io/Stream.java
@@ -18,6 +18,7 @@
 
 package totalcross.io;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import totalcross.sys.*;
@@ -148,4 +149,32 @@ public abstract class Stream extends Connection
    public InputStream wrapInputStream() {
 	   return new WrapInputStream(this);
    }
+}
+
+class WrapInputStream extends InputStream {
+	private Stream stream;
+
+	WrapInputStream(Stream stream) {
+		this.stream = stream;
+	}
+
+	@Override
+	public int read() throws IOException {
+		byte[] b = new byte[1];
+		if (read(b) == -1) {
+			return -1;
+		}
+		
+		return b[0];
+	}
+	
+	@Override
+	public int read(byte b[], int off, int len) throws IOException {
+		try {
+			return stream.readBytes(b, off, len);
+		} catch (totalcross.io.IOException e) {
+			throw new java.io.IOException(e);
+		}
+	}
+	
 }
