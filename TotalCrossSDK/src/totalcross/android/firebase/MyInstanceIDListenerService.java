3a150e26a04edaf77498ab88a4087faae5c2b650
Jefferson Quesado <jeff.quesado@gmail.com>
2017-02-01 14:23:40 -0300

    Pondo o Token Receiver para receber as paradas

--- TotalCrossSDK/src/totalcross/android/firebase/MyInstanceIDListenerService.java
+++ TotalCrossSDK/src/totalcross/android/firebase/MyInstanceIDListenerService.java
@@ -1,15 +1,36 @@
 package totalcross.android.firebase;
 
-public class MyInstanceIDListenerService extends InstanceIDListenerService
-{
-   /**
-    * Called if InstanceID token is updated. This may occur if the security of the previous token had been compromised.
-    * This call is initiated by the InstanceID provider.
-    */
+import static totalcross.android.firebase.LocalBroadcastManager.TAG;
 
-   public void onTokenRefresh()
-   {
-      // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
-      startService(new Intent(this, GCMTokenReceiver.class));
+import android.content.Intent;
+import android.util.Log;
+import firebase.iid.FirebaseInstanceIdService
+import firebase.iid.FirebaseInstanceId
+
+public class MyInstanceIDListenerService extends FirebaseInstanceIdService
+{
+
+   /**
+    * Based on https://developers.google.com/cloud-messaging/android/android-migrate-fcm
+	*/
+	
+   /**
+    * Called if InstanceID token is updated. This may occur if the security of
+    * the previous token had been compromised. Note that this is also called
+    * when the InstanceID token is initially generated, so this is where
+    * you retrieve the token.
+    */
+   // [START refresh_token]
+   @Override
+   public void onTokenRefresh() {
+      // Get updated InstanceID token.
+      String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+      Log.d(TAG, "Refreshed token: " + refreshedToken);
+      // TODO: Implement this method to send any registration to your app's servers.
+      // sendRegistrationToServer(refreshedToken);
+	  
+	  // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
+      startService(new Intent(this, FirebaseTokenReceiver.class));
    }
+
 }
