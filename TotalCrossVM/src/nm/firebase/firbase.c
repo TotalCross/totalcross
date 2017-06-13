6094993ba9fd9c31ade30b5801d3be386f617da7
Jefferson Quesado <jeff.quesado@gmail.com>
2017-06-13 11:03:56 -0300

    Getting the token from the new TC FirebaseUtils
    Instead of getting from bare Firebase, I get from this utility method so that I can manage what FirebaseApp Iam using...
    
    I hope to change to the default FirebaseApp ASAP!!

--- TotalCrossVM/src/nm/firebase/firbase.c
+++ TotalCrossVM/src/nm/firebase/firbase.c
@@ -20,10 +20,8 @@ TC_API void tfiFII_getToken(NMParams p)
 	fprintf(stderr, "%s %d\n", __FILE__, __LINE__);
 	JNIEnv* env = getJNIEnv();
 
-	jmethodID getInstance = (*env)->GetStaticMethodID(env, jFirebaseInstanceId, "getInstance", "()Lcom/google/firebase/iid/FirebaseInstanceId");
-	jmethodID getToken = (*env)->GetMethodID(env, jFirebaseInstanceId, "getToken", "()Ljava/lang/String");
-	jobject firebase_instance = (*env)->CallStaticObjectMethod(env, jFirebaseInstanceId, getInstance);
-	jobject jtoken = (*env)->CallObjectMethod(firebase_instance, getToken);
+	jmethodID getToken = (*env)->GetStaticMethodID(env, jTcFirebaseUtils, "getTokenFromRegisteredFirebaseApp", "()Ljava/lang/String");
+	jobject jtoken = (*env)->CallStaticObjectMethod(env, jTcFirebaseUtils, getToken);
 
 	if (jtoken == null) {
 		p->retO = null;
@@ -34,7 +32,7 @@ TC_API void tfiFII_getToken(NMParams p)
 		p->retO = tctoken;
 		setObjectLock(p->retO, UNLOCKED);
 
-		(*env)->ReleaseStringUTFChars(env, jtoken, szDisplayOriginatingAddress);
+		(*env)->ReleaseStringUTFChars(env, jtoken, sztoken);
 	}
 #else
 #error "ERRO NAO DEFINIDO ANDROID"
