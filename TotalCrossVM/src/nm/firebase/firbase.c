176442f3f5260e44d8624a7265e526d4da2b9d26
Jefferson Quesado <jeff.quesado@gmail.com>
2017-06-13 16:35:35 -0300

    Forgot ; ¬¬

--- TotalCrossVM/src/nm/firebase/firbase.c
+++ TotalCrossVM/src/nm/firebase/firbase.c
@@ -19,7 +19,7 @@ TC_API void tfiFII_getToken(NMParams p)
 	fprintf(stderr, "%s %d\n", __FILE__, __LINE__);
 	JNIEnv* env = getJNIEnv();
 
-	jmethodID getToken = (*env)->GetStaticMethodID(env, jTcFirebaseUtils, "getTokenFromRegisteredFirebaseApp", "()Ljava/lang/String");
+	jmethodID getToken = (*env)->GetStaticMethodID(env, jTcFirebaseUtils, "getTokenFromRegisteredFirebaseApp", "()Ljava/lang/String;");
 	jobject jtoken = (*env)->CallStaticObjectMethod(env, jTcFirebaseUtils, getToken);
 
 	if (jtoken == null) {
