a8453f5338d3ac207c023a04fbeebd2b0c228617
Jefferson Quesado <jeff.quesado@gmail.com>
2017-06-19 16:32:02 -0300

    Referencing proper Firebase documentation

--- TotalCrossSDK/src/main/java/totalcross/firebase/iid/FirebaseInstanceId.java
+++ TotalCrossSDK/src/main/java/totalcross/firebase/iid/FirebaseInstanceId.java
@@ -1,5 +1,19 @@
 package totalcross.firebase.iid;
 
+/**
+ * Use it like https://firebase.google.com/docs/reference/android/com/google/firebase/iid/FirebaseInstanceId
+ * 
+ * In the enabled device, it will search for the default FirebaseApp name that TotalCross uses and return
+ * it's FirebaseInstanceId instance token. If in a device that is not enabled Firebase (be it lack of
+ * configuration file or not yet binded with TotalCross), it will return null.
+ * 
+ * If you are going to deliver a unicast push message, you must retrieve the must recent token and use it.
+ * 
+ * Currently suported platforms:
+ * <ul>
+ * 		<li>Android</li>
+ * </ul>
+ */
 public class FirebaseInstanceId {
 	private static FirebaseInstanceId instance = new FirebaseInstanceId();
 	
--- TotalCrossSDK/src/main/java/totalcross/firebase/iid/FirebaseInstanceIdService.java
+++ TotalCrossSDK/src/main/java/totalcross/firebase/iid/FirebaseInstanceIdService.java
@@ -1,5 +1,15 @@
 package totalcross.firebase.iid;
 
+/**
+ * Use it like https://firebase.google.com/docs/reference/android/com/google/firebase/iid/FirebaseInstanceIdService
+ * 
+ * Instead of declaring it in the manifest, you should override MainWindow.initFirebaseInstanceIdService() with
+ * your implementation.
+ * 
+ * If you are going to deliver a unicast push message, you must retrieve the must recent token and use it.
+ * 
+ * Currently not bound with native Firebase
+ */
 public class FirebaseInstanceIdService {
 
 	public void onTokenRefresh() {
