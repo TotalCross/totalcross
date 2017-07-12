620a99ffc71cf6c40258804713dbb9c0cc137360
Fabio Sobral <fabio.sobral@totalcross.com>
2017-07-12 15:00:13 -0300

    extracts class JarClassPathLoader from Deploy to its own file

--- TotalCrossSDK/src/main/java/tc/Deploy.java
+++ TotalCrossSDK/src/main/java/tc/Deploy.java
@@ -13,15 +13,15 @@
 
 package tc;
 
-import totalcross.sys.*;
-import totalcross.util.*;
-
 import java.io.*;
-import java.lang.reflect.*;
-import java.net.*;
-import tc.tools.*;
-import tc.tools.converter.*;
+
+import tc.tools.JarClassPathLoader;
+import tc.tools.RegisterSDK;
+import tc.tools.converter.J2TC;
 import tc.tools.deployer.*;
+import totalcross.sys.Convert;
+import totalcross.util.ElementNotFoundException;
+import totalcross.util.IntHashtable;
 
 public class Deploy
 {
@@ -166,49 +166,6 @@ public class Deploy
       JarClassPathLoader.addFile(DeploySettings.etcDir + "tools/ipa/dd-plist.jar");
    }
 
-   /**
-    * Utility class that dynamically loads a jar file into the Deploy classpath.<br>
-    * It uses reflection to grant access to the loaded jar, a little hackish but that's the easiest way of doing it.<br>
-    * Another option would be to iterate through the jar entries and load classes one by one, which is not much better
-    * than this IMHO. So I decided to go with the easiest-to-implement solution.
-    * 
-    * @author Fabio Sobral
-    * @since TotalCross 1.15
-    */
-   //flsobral@tc115: just a mark for quick search, see class documentation above.
-   public static class JarClassPathLoader
-   {
-      private static final Class<?>[] parameters = new Class[] { URL.class };
-
-      public static void addFile(String s) throws java.io.IOException
-      {
-         java.io.File f = new java.io.File(s);
-         addFile(f);
-      }
-
-      public static void addFile(java.io.File f) throws java.io.IOException
-      {
-         addURL(f.toURI().toURL());
-      }
-
-      public static void addURL(URL u) throws java.io.IOException
-      {
-         URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-         Class<?> sysclass = URLClassLoader.class;
-
-         try
-         {
-            Method method = sysclass.getDeclaredMethod("addURL", parameters);
-            method.setAccessible(true);
-            method.invoke(sysloader, new Object[] { u });
-         }
-         catch (Throwable t)
-         {
-            throw new java.io.IOException("Error, could not add URL to system classloader", t);
-         }
-      }
-   }
-   
    private void showException(Throwable e, String extraMsg)
    {
       System.out.println();
