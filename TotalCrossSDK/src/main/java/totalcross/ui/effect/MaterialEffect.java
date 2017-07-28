28792d1bc6cdcc203a7b86fa4b915b95f82d7591
Guilherme Campos Hazan <guich@superwaba.com.br>
2017-07-28 11:32:07 +0000

    MaterialEffect: corrected test.

--- TotalCrossSDK/src/main/java/totalcross/ui/effect/MaterialEffect.java
+++ TotalCrossSDK/src/main/java/totalcross/ui/effect/MaterialEffect.java
@@ -71,10 +71,10 @@ public class MaterialEffect extends UIEffects implements PenListener, TimerListe
         w = target.getEffectW();
         h = target.getEffectH();
       }
-      int tw = target.getWidth();  // dont let it go beyond screen size
-      int th = target.getHeight();
-      if (w > tw) {x = 0; w = Convert.min(Settings.screenWidth,  tw);}
-      if (h > th) {y = 0; h = Convert.min(Settings.screenHeight, th);}
+      int tw = Convert.min(Settings.screenWidth,  target.getWidth());  // dont let it go beyond screen size
+      int th = Convert.min(Settings.screenHeight, target.getHeight());
+      if (w > tw) {x = 0; w = tw;}
+      if (h > th) {y = 0; h = th;}
       if (matImg == null || matImg.getWidth() != w || matImg.getHeight() != h) {
         try {matImg = new Image(w,h);} catch (Throwable t) {t.printStackTrace();}
       }
