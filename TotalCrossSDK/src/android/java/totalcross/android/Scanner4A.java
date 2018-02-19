ccc92d669a2b2854d21e540ae2b89c5c2587dd90
Fabio Sobral <fabio.sobral@totalcross.com>
2018-02-19 13:48:33 -0300

    source formatting

--- TotalCrossSDK/src/android/java/totalcross/android/Scanner4A.java
+++ TotalCrossSDK/src/android/java/totalcross/android/Scanner4A.java
@@ -21,47 +21,41 @@ import totalcross.android.scanners.*;
 /**
  * Scanner class for Android.
  */
-public class Scanner4A
-{ 
-   public static IScanner scanner;
-   
-   static boolean scannerActivate()
-   {
-      String id = Settings4A.deviceId.toLowerCase();
-      if (id.contains("honeywell"))
-         scanner = new HoneywellScanner();
-      else
-      if (id.equalsIgnoreCase("intermec") || id.contains("foxconn"))
-         scanner = new IntermecScanner();
-      else if (id.contains("motorola")) {
-        try {
-          if (Class.forName("com.symbol.emdk.EMDKManager") != null) {
-            scanner = new MotorolaScanner();
-          }
-        } catch (ClassNotFoundException e) {
+public class Scanner4A {
+  public static IScanner scanner;
+
+  static boolean scannerActivate() {
+    String id = Settings4A.deviceId.toLowerCase();
+    if (id.contains("honeywell")) {
+      scanner = new HoneywellScanner();
+    } else if (id.equalsIgnoreCase("intermec") || id.contains("foxconn")) {
+      scanner = new IntermecScanner();
+    } else if (id.contains("motorola")) {
+      try {
+        if (Class.forName("com.symbol.emdk.EMDKManager") != null) {
+          scanner = new MotorolaScanner();
         }
+      } catch (ClassNotFoundException e) {
       }
-      return scanner != null ? scanner.scannerActivate() : false;
-   }
-   
-   static boolean setBarcodeParam(int barcodeType, boolean enable)
-   {
-      return scanner != null && scanner.setBarcodeParam(barcodeType, enable);
-   }
-   
-   static void setParam(String what, String value)
-   {
-      if (scanner != null)
-         scanner.setParam(what, value);
-   }
-   
-   static String getData()
-   {
-      return scanner == null ? null : scanner.getData();
-   }
-   
-   static boolean deactivate()
-   {
-      return scanner != null && scanner.deactivate();
-   }
+    }
+    return scanner != null ? scanner.scannerActivate() : false;
+  }
+
+  static boolean setBarcodeParam(int barcodeType, boolean enable) {
+    return scanner != null && scanner.setBarcodeParam(barcodeType, enable);
+  }
+
+  static void setParam(String what, String value) {
+    if (scanner != null) {
+      scanner.setParam(what, value);
+    }
+  }
+
+  static String getData() {
+    return scanner == null ? null : scanner.getData();
+  }
+
+  static boolean deactivate() {
+    return scanner != null && scanner.deactivate();
+  }
 }
