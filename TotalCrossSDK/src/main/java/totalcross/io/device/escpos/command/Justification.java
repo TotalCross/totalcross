1df3a5c372ced2e05468439b48d212bc4407eeb0
Fabio Sobral <fabio.sobral@totalcross.com>
2018-08-15 18:26:37 -0300

    fixes values for Justification constants CENTER and RIGHT

--- TotalCrossSDK/src/main/java/totalcross/io/device/escpos/command/Justification.java
+++ TotalCrossSDK/src/main/java/totalcross/io/device/escpos/command/Justification.java
@@ -2,6 +2,6 @@ package totalcross.io.device.escpos.command;
 
 public interface Justification {
   public static final byte LEFT = 0x00;
-  public static final byte CENTER = 0x00;
-  public static final byte RIGHT = 0x00;
+  public static final byte CENTER = 0x01;
+  public static final byte RIGHT = 0x02;
 }
