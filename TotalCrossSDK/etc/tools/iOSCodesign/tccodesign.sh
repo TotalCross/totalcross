bdca4decf67821ffd707d8ec646a58dcf816f739
Fabio <flsobral@gmail.com>
2020-01-23 14:52:40 -0300

    updated to be compatible with MacOS X Catalina

--- TotalCrossSDK/etc/tools/iOSCodesign/tccodesign.sh
+++ TotalCrossSDK/etc/tools/iOSCodesign/tccodesign.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 # Parse variables
 POSITIONAL=()
@@ -93,10 +93,13 @@ unzip -jo "${ipaDest}" "${appDir}/*.tcz" -d "${tempDir}/tczs"
 # Extract files in pkg folder
 unzip -jo "${ipaDest}" "${appDir}/pkg/*" -d "${tempDir}/pkg"
 
+#New Info.plist
+Infoplist="${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app/Info.plist"
+
 # Copy tczs to ${tempDir}/${appName}.xcarquive/Products/Applications/*.app/
 cp "${tempDir}"/tczs/*.tcz "${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app/"
 # Copy Infoplist to ${tempDir}/${appName}.xcarquive/Products/Applications/*.app/
-cp "${tempDir}/Info.plist" "${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app/"
+cp "${tempDir}/Info.plist" ${Infoplist}
 # Copy GoogleService-Info
 cp "${tempDir}/GoogleService-Info.plist" "${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app/"
 # Copy files in pkg folder
@@ -113,10 +116,11 @@ cp "${tempDir}"/assets/Assets.car "${tempDir}/${appName}.xcarchive/Products/Appl
 cp "${tempDir}"/assets/AppIcon* "${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app/"
 
 # Copy CFBundleIcons and *ipad to Info.plist
-defaults delete "${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app/Info.plist" CFBundleIconFiles
-/usr/libexec/PlistBuddy -x -c "Merge ${tempDir}/assets/partial.plist" "${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app/Info.plist"
-defaults write "${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app/Info.plist" DTPlatformVersion 12.0
-defaults write "${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app/Info.plist" DTSDKName iphoneos12.0
+/usr/libexec/PlistBuddy  ${verbose} -c "Delete :CFBundleIconFiles" ${Infoplist}
+/usr/libexec/PlistBuddy ${verbose} -c "Merge ${tempDir}/assets/partial.plist" ${Infoplist}
+
+/usr/libexec/PlistBuddy ${verbose} -c "Set :DTPlatformVersion 12.0" ${Infoplist}
+/usr/libexec/PlistBuddy ${verbose} -c "Set :DTSDKName iphoneos12.0" ${Infoplist} 
 
 # Modify entitlements for push notification
 cp "${workDir}/TotalCross.entitlements" "${tempDir}/TotalCross.entitlements"
@@ -136,13 +140,13 @@ if [ "${apsType}" = "development" ] || [ "${apsType}" = "production" ] # mobile 
 then
 /usr/bin/codesign -f -s "${certificateName}" --entitlements "${tempDir}/TotalCross.entitlements" "${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app"
 else
-defaults delete "${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app/Info.plist" UIBackgroundModes
+/usr/libexec/PlistBuddy ${verbose} -c "Delete :UIBackgroundModes" ${Infoplist}
 /usr/bin/codesign -f -s "${certificateName}" "${tempDir}/${appName}.xcarchive/Products/Applications/${appName}.app"
 fi
 
 #Build ExportOptions.plist
 cp "${workDir}/ExportOptions.plist" "${tempDir}/"
-bundleID=$(defaults read "${tempDir}/Info" CFBundleIdentifier)
+bundleID=$(/usr/libexec/PlistBuddy -c "Print :CFBundleIdentifier" "${tempDir}/Info.plist")
 provName=$(/usr/libexec/PlistBuddy -c 'Print :Name' /dev/stdin <<< $(security cms -D -i "${provisionFile}"))
 provUUID=$(/usr/libexec/PlistBuddy -c 'Print :UUID' /dev/stdin <<< $(security cms -D -i "${provisionFile}"))
 teamID=$(/usr/libexec/PlistBuddy -c 'Print :Entitlements:com.apple.developer.team-identifier' /dev/stdin <<< $(security cms -D -i "${provisionFile}"))
@@ -155,24 +159,26 @@ echo "      bundleID: ${bundleID}"
 echo "      teamID: ${teamID}"
 echo    ================================================================
 
-defaults write "${tempDir}/ExportOptions" provisioningProfiles "<dict><key>${bundleID}</key><string>${provName}</string></dict>"
-defaults write "${tempDir}/ExportOptions" teamID $teamID
+/usr/libexec/PlistBuddy ${verbose} -c "Add :provisioningProfiles:${bundleID} string '${provName}'" "${tempDir}/ExportOptions.plist"
+/usr/libexec/PlistBuddy ${verbose} -c "Delete :provisioningProfiles:com.totalcross.sdk" "${tempDir}/ExportOptions.plist"
+/usr/libexec/PlistBuddy ${verbose} -c "Set teamID ${teamID}" "${tempDir}/ExportOptions.plist"
 signingCertificate=$(echo "${certificateName}" | sed -e 's/\(.*\)\:.*/\1/')
 echo $signingCertificate
-defaults write "${tempDir}/ExportOptions" signingCertificate "${signingCertificate}"
-defaults write "${tempDir}/ExportOptions" method $method
+/usr/libexec/PlistBuddy ${verbose} -c "Set :signingCertificate '${signingCertificate}'" "${tempDir}/ExportOptions.plist"
+/usr/libexec/PlistBuddy ${verbose} -c "Set :method '${method}'" "${tempDir}/ExportOptions.plist"
 
 #Modifying Info.plist into root of App.xcarchive
-versionString=$(defaults read "${tempDir}/Info" CFBundleShortVersionString)
-version=$(defaults read "${tempDir}/Info" CFBundleVersion)
+versionString=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "${tempDir}/Info.plist")
+version=$(/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" "${tempDir}/Info.plist")
 
-defaults write "${tempDir}/${appName}.xcarchive/Info" ApplicationProperties -dict-add SigningIdentity "'${certificateName}'"
-defaults write "${tempDir}/${appName}.xcarchive/Info" ApplicationProperties -dict-add ApplicationPath "Applications/${appName}.app"
-defaults write "${tempDir}/${appName}.xcarchive/Info" ApplicationProperties -dict-add CFBundleIdentifier "${bundleID}"
-defaults write "${tempDir}/${appName}.xcarchive/Info" ApplicationProperties -dict-add Team "${teamID}"
-defaults write "${tempDir}/${appName}.xcarchive/Info" ApplicationProperties -dict-add CFBundleShortVersionString "${versionString}"
-defaults write "${tempDir}/${appName}.xcarchive/Info" ApplicationProperties -dict-add CFBundleVersion "${version}"
-defaults write "${tempDir}/${appName}.xcarchive/Info" Name "${appName}"
-defaults write "${tempDir}/${appName}.xcarchive/Info" SchemeName "${appName}"
+
+/usr/libexec/PlistBuddy ${verbose} -c "Set :ApplicationProperties:SigningIdentity '${certificateName}'" "${tempDir}/${appName}.xcarchive/Info.plist"
+/usr/libexec/PlistBuddy ${verbose} -c "Set :ApplicationProperties:ApplicationPath 'Applications/${appName}.app'" "${tempDir}/${appName}.xcarchive/Info.plist"
+/usr/libexec/PlistBuddy ${verbose} -c "Set :ApplicationProperties:CFBundleIdentifier '${bundleID}'" "${tempDir}/${appName}.xcarchive/Info.plist"
+/usr/libexec/PlistBuddy ${verbose} -c "Set :ApplicationProperties:Team '${teamID}'" "${tempDir}/${appName}.xcarchive/Info.plist"
+/usr/libexec/PlistBuddy ${verbose} -c "Set :ApplicationProperties:CFBundleShortVersionString '${versionString}'" "${tempDir}/${appName}.xcarchive/Info.plist"
+/usr/libexec/PlistBuddy ${verbose} -c "Set :ApplicationProperties:CFBundleVersion '${version}'" "${tempDir}/${appName}.xcarchive/Info.plist"
+/usr/libexec/PlistBuddy ${verbose} -c "Set :Name '${appName}'" "${tempDir}/${appName}.xcarchive/Info.plist"
+/usr/libexec/PlistBuddy ${verbose} -c "Set :SchemeName '${appName}'" "${tempDir}/${appName}.xcarchive/Info.plist"
 
 xcodebuild -exportArchive -archivePath "${tempDir}/${appName}.xcarchive" -exportPath "${output}" -exportOptionsPlist "${tempDir}/ExportOptions.plist"
