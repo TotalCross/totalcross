1cf41a76c6f1fe7e8b8cfcd6a72d01bdaf4c96cf
Fabio <flsobral@gmail.com>
2020-03-11 18:46:40 -0300

    adds missing android build files and test sources

---
+++ TotalCrossVM/src/tests/java/TestExt.java
@@ -0,0 +1,25 @@
+/*********************************************************************************
+ *  TotalCross Software Development Kit                                          *
+ *  Copyright (C) 2000-2012 SuperWaba Ltda.                                      *
+ *  All Rights Reserved                                                          *
+ *                                                                               *
+ *  This library and virtual machine is distributed in the hope that it will     *
+ *  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of    *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                         *
+ *                                                                               *
+ *********************************************************************************/
+
+
+
+public class TestExt extends Object
+{
+   public int    i=0x12345678;
+   public String o="TotalCross";
+   public double d=-1234.567;
+   public long   l=-0x12345678ABCEL;
+
+   public static int    si=0x12345678;
+   public static String so="TotalCross";
+   public static double sd=-1234.567;
+   public static long   sl=-0x12345678ABCEL;
+}
