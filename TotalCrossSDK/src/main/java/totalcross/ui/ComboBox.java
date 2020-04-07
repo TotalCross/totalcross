7c267c1a579eef1852d1a879c047bea234fca958
Fabio <flsobral@gmail.com>
2020-04-07 11:30:36 -0300

    changes license from lgpl 3.0 to lgpl 2.1

--- TotalCrossSDK/src/main/java/totalcross/ui/ComboBox.java
+++ TotalCrossSDK/src/main/java/totalcross/ui/ComboBox.java
@@ -8,10 +8,10 @@
  *  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of    *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                         *
  *                                                                               *
- *  This file is covered by the GNU LESSER GENERAL PUBLIC LICENSE VERSION 3.0    *
+ *  This file is covered by the GNU LESSER GENERAL PUBLIC LICENSE VERSION 2.1    *
  *  A copy of this license is located in file license.txt at the root of this    *
  *  SDK or can be downloaded here:                                               *
- *  http://www.gnu.org/licenses/lgpl-3.0.txt                                     *
+ *  http://www.gnu.org/licenses/lgpl-2.1.txt                                     *
  *                                                                               *
  *********************************************************************************/
 
