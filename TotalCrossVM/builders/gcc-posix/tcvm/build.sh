654f4155872046e84fe1f98cf4648c8a000cc90b
Allan C <acmlira@gmail.com>
2020-04-16 10:45:51 -0300

    Renames docker image and updates launcher build

--- TotalCrossVM/builders/gcc-posix/tcvm/build.sh
+++ TotalCrossVM/builders/gcc-posix/tcvm/build.sh
@@ -9,7 +9,7 @@ sudo docker run \
 -v $WORKDIR/../../../src:/src \
 -e SRCDIR=/../../../src \
 -e LIBS="-L. -lskia -lstdc++ -lpthread -lfontconfig -lGL -lSDL2main -lSDL2" \
--t totalcross/cross-compile:bionic \
+-t totalcross/amd64-cross-compile:bionic \
 bash -c "make  -j$(($(nproc) + 2)) -f /build/Makefile"
 
 # bash -c "make  -j$(($(nproc) + 2)) -f ${WORKDIR}/Makefile"
