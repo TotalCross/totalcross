7188664a66893e1f24d9d785499a326ca0f5a908
Ricardo <ricardo.bragana@edu.unifor.br>
2020-04-22 11:41:08 -0300

    Reverting wrong change on last commit

--- TotalCrossSDK/src/main/java/jdkcompat/nio/ByteBuffer4D.java
+++ TotalCrossSDK/src/main/java/jdkcompat/nio/ByteBuffer4D.java
@@ -2,6 +2,10 @@ package jdkcompat.nio;
 
 public class ByteBuffer4D {
 
+    byte[] array;
+    int offset;
+    int length;
+    
     // static public ByteBuffer4D wrap(byte[] array, int offset, int length) {
     //     return new ByteBufferImpl(array, offset, length);
     // }
