e88c4b80d53fae1cc2564e889e0675e863d44836
Allan C <acmlira@gmail.com>
2020-04-29 16:18:25 -0300

    Dynamically linking SDL

--- TotalCrossVM/builders/gcc-linux-arm/docker-builder-image/build.sh
+++ TotalCrossVM/builders/gcc-linux-arm/docker-builder-image/build.sh
@@ -2,6 +2,6 @@
 export DOCKER_CLI_EXPERIMENTAL=enabled
 export DOCKER_BUILDKIT=1
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-sudo docker buildx create --use --name armbuilder
-sudo docker buildx inspect --bootstrap
-sudo docker buildx build --platform linux/arm/v7 --load -t totalcross/cross-compile .
+docker buildx create --use --name armbuilder
+docker buildx inspect --bootstrap
+docker buildx build --platform linux/arm/v7 --load -t totalcross/cross-compile .
