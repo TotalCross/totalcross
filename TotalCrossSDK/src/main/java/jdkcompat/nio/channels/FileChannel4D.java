6b1490cdba0e6d8df7d468f9dc1669f684c78221
Ricardo <ricardo.bragana@edu.unifor.br>
2020-05-14 14:57:45 -0300

    Removing not used java classes and adding licenses.

--- TotalCrossSDK/src/main/java/jdkcompat/nio/channels/FileChannel4D.java
+++ TotalCrossSDK/src/main/java/jdkcompat/nio/channels/FileChannel4D.java
@@ -1,3 +1,8 @@
+// Copyright (C) 2000-2013 SuperWaba Ltda.
+// Copyright (C) 2014-2020 TotalCross Global Mobile Platform Ltda.
+//
+// SPDX-License-Identifier: LGPL-2.1-only
+
 package jdkcompat.nio.channels;
 
 import java.io.Closeable;
--- TotalCrossVM/src/nm/nio/channels/FileChannelImpl.h
+++ TotalCrossVM/src/nm/nio/channels/FileChannelImpl.h
@@ -1,3 +1,7 @@
+// Copyright (C) 2000-2013 SuperWaba Ltda.
+// Copyright (C) 2014-2020 TotalCross Global Mobile Platform Ltda.
+//
+// SPDX-License-Identifier: LGPL-2.1-only
 
 #ifndef Runtime_h
 #define Runtime_h
