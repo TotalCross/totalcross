d4579996a6422a750ef37185035c6eb53ef577b8
Panayotis Katsaloulis <panayotis@panayotis.com>
2020-05-26 18:04:10 +0300

    change osx build script

--- .travis/build.osx.sh
+++ .travis/build.osx.sh
@@ -6,8 +6,13 @@ source $TRAVIS_BUILD_DIR/.travis/common.sh
 # Break when error occurs
 set -e
 
-cd $TRAVIS_BUILD_DIR/TotalCrossVM/builders/xcode ; bash -C ./build.sh
 
+
+cd $TRAVIS_BUILD_DIR/TotalCrossVM/builders/xcode
+
+pod install
+
+# Create export options
 cat >ExportOptions.plist <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -16,28 +21,49 @@ cat >ExportOptions.plist <<EOF
  <key>compileBitcode</key>
  <true/>
  <key>method</key>
- <string>enterprise</string>
+ <string>development</string>
  <key>provisioningProfiles</key>
  <dict>
   <key>com.totalcross.sdk</key>
-  <string>TotalCross Wildcard</string>
+  <string>TotalCross_WildCard_Development</string>
  </dict>
  <key>signingCertificate</key>
- <string>iPhone Distribution</string>
+ <string>iPhone Developer</string>
  <key>signingStyle</key>
  <string>manual</string>
  <key>stripSwiftSymbols</key>
  <true/>
  <key>teamID</key>
- <string>$TC_TEAM_ID</string>
+ <string>R8FWE2RXQV</string>
  <key>thinning</key>
  <string>&lt;none&gt;</string>
 </dict>
 </plist>
 EOF
 
-cd $TRAVIS_BUILD_DIR
-xcodebuild -exportArchive -archivePath TotalCrossVM/builders/xcode/build/Release-iphoneos/TotalCross.xcarchive -exportPath TotalCrossVM/builders/xcode/build/Release-iphoneos -exportOptionsPlist TotalCrossVM/builders/xcode/ExportOptions.plist
-mkdir -p TotalCrossSDK/dist/vm/ios && mv TotalCrossVM/builders/xcode/build/Release-iphoneos/TotalCross*.ipa TotalCrossSDK/dist/vm/ios/TotalCross.ipa
+# Create keychain
+echo $P12_APPLE_CONTENT | base64 --decode > apple-keysign.p12
+security create-keychain -p $P12_APPLE_PASS build.keychain
+security default-keychain -s build.keychain
+security unlock-keychain -p $P12_APPLE_PASS build.keychain
+security import apple-keysign.p12 -k build.keychain -P $P12_APPLE_PASS -T /usr/bin/codesign
+security find-identity -v
+security set-key-partition-list -S 'apple-tool:,apple:' -s -k $P12_APPLE_PASS build.keychain
+rm apple-keysign.p12
+
+# Copy Mobile provision
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+echo $MOBILE_PROVISION_CONTENT | base64 --decode | bunzip2 >TotalCross_WildCard_Development.mobileprovision
+uuid=`grep UUID -A1 -a TotalCross_WildCard_Development.mobileprovision | grep -io "[-A-F0-9]\{36\}"`
+cp TotalCross_WildCard_Development.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/$uuid.mobileprovision
+ls -l ~/Library/MobileDevice/Provisioning\ Profiles
+
+xcodebuild -workspace TotalCross.xcworkspace -scheme TotalCross archive -archivePath build/TotalCross.xcarchive CODE_SIGN_IDENTITY="Apple Development: Italo Medeiros Bruno" PROVISIONING_PROFILE="TotalCross_WildCard_Development" teamID=$TC_TEAM_ID -configuration Debug -quiet
+
+xcodebuild -exportArchive -archivePath build/TotalCross.xcarchive -exportPath build/TotalCross.ipa -exportOptionsPlist ExportOptions.plist
 
 
+# Copy files to collect
+mkdir -p /tmp/output/osx
+cp build/TotalCross.ipa /tmp/output/osx/
+cp build/TotalCross.xcarchive /tmp/output/osx/
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source "/Users/teras/.sdkman/bin/sdkman-init.sh"
+sdk use java 14.0.1.hs-adpt
+
+java -version
