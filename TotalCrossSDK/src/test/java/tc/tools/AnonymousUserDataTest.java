57e4513708140423d77ca456d4ad718e396f976a
italo yeltsin <br.yeltsin@gmail.com>
2020-09-23 11:54:20 -0300

    statistics: refact AnonymousUserData class and tests
    Removes duplicated code and moves field POPUP_TEXT
    to DefaulResponseRequester in order to have a better
    cohesion.

--- TotalCrossSDK/src/test/java/tc/tools/AnonymousUserDataTest.java
+++ TotalCrossSDK/src/test/java/tc/tools/AnonymousUserDataTest.java
@@ -68,16 +68,7 @@ public class AnonymousUserDataTest {
         // Create config file
         File f = new File(configPath);
         String existingToken = "d61cfa55-cb26-49f3-8468-2a91ed94971f"; // existing on stage server
-        f.createNewFile();
-        try {
-            FileWriter myWriter = new FileWriter(f);
-            myWriter.write("{\"userAcceptedToProvideAnonymousData\":true," +
-                    "\"uuid\": \"" + existingToken + "\"}");
-            myWriter.close();
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-
+        writeConfigFile(existingToken, true);
         // Use config file
         anonymousUserData.loadConfiguration();
         anonymousUserData.launcher("/src", "200x800");
@@ -91,18 +82,8 @@ public class AnonymousUserDataTest {
     @Test
     void shouldEraseTheExistingInvalidUUIDAndGetANewValidOne() throws IOException {
         // Create config file
-        File f = new File(configPath);
         String existingInvalidUuid = "52f265eb-8d36-4ffe-b2c5-3c4affb278fe"; // existing invalid
-        f.createNewFile();
-        try {
-            FileWriter myWriter = new FileWriter(f);
-            myWriter.write("{\"userAcceptedToProvideAnonymousData\":true," +
-                    "\"uuid\": \"" + existingInvalidUuid + "\"}");
-            myWriter.close();
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-
+        writeConfigFile(existingInvalidUuid, true);
         // Use config file
         anonymousUserData.loadConfiguration();
         anonymousUserData.launcher("/src", "200x800");
@@ -113,6 +94,19 @@ public class AnonymousUserDataTest {
             "should use not be existing the existing invalid token");
         assertTrue(anonymousUserData.checkUUID(newValidUuid), "new token should be valid");
         
+    }
+
+    private void writeConfigFile(String uuid, boolean userAcceptedToContribute) throws IOException {
+        File f = new File(configPath);
+        f.createNewFile();
+        try {
+            FileWriter myWriter = new FileWriter(f);
+            myWriter.write("{\"userAcceptedToProvideAnonymousData\": " + userAcceptedToContribute + "," +
+                    "\"uuid\": \"" + uuid + "\"}");
+            myWriter.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
  
 }
