cde2d86090d98b2488a778ea24dbe2f9151760f5
Fabio <flsobral@gmail.com>
2021-01-08 16:55:15 -0300

    android: target sdk version 29

--- TotalCrossVM/builders/droid/build.sh
+++ TotalCrossVM/builders/droid/build.sh
@@ -7,6 +7,6 @@ mkdir -p ${WORKDIR}/.gradle
 # execute docker run
 docker run                                                  \
     -v ${WORKDIR}/../../:/vm                                \
-    -v ${WORKDIR}/.gradle:/root/.gradlew                    \
-    -it fabernovel/android:api-28-gcloud-ndk-v1.2.0         \
+    -v ${WORKDIR}/.gradle:/root/.gradle                     \
+    -it fabernovel/android:api-29-ndk-v1.2.0                \
     bash -c "cd /vm/builders/droid && ./gradlew assembleRelease"
