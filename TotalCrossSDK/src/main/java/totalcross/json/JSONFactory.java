309579c858dabe7a49a6606b8571fd93e5783391
Fabio <flsobral@gmail.com>
2021-07-19 19:54:12 -0300

    chore: caches Class' methods after first call to parse
    It makes sense to cache Class and Method references within JSONFactory,
    because it will most likely be used for the same class several times.
    Especially when considering that classes are locked objects and their
    methods cached after the first getMethods call.

--- TotalCrossSDK/src/main/java/totalcross/json/JSONFactory.java
+++ TotalCrossSDK/src/main/java/totalcross/json/JSONFactory.java
@@ -10,7 +10,10 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
     The JSONFactory class helps converting json objects into Java objects, using reflection.
@@ -50,6 +53,8 @@ import java.util.List;
     You may also retrieve a list or an array. See the JSONSample in the TotalCrossAPI.
  */
 public class JSONFactory {
+  private static Map<Class<?>, Map<String, Method>> classes = new HashMap<>();
+
   public static <T> List<T> asList(String json, Class<T> classOfT) throws InstantiationException,
       IllegalAccessException, IllegalArgumentException, InvocationTargetException, JSONException, ArrayIndexOutOfBoundsException, NoSuchMethodException, SecurityException {
     List<T> list = new ArrayList<T>();
@@ -123,82 +128,97 @@ public class JSONFactory {
     }
     T object = null;
     try {
-        object = classOfT.newInstance();
+      object = classOfT.newInstance();
     } catch (InstantiationException e) {
-        if (outerObject != null && classOfT.getName().indexOf(outerObject.getClass().getName()) != -1) {
-            Constructor<T> constructorOfT = classOfT.getDeclaredConstructor(outerObject.getClass());
-            if (constructorOfT != null) {
-                object = constructorOfT.newInstance(outerObject);
-            }
+      if (outerObject != null && classOfT.getName().indexOf(outerObject.getClass().getName()) != -1) {
+        Constructor<T> constructorOfT = classOfT.getDeclaredConstructor(outerObject.getClass());
+        if (constructorOfT != null) {
+          object = constructorOfT.newInstance(outerObject);
         }
-        if (object == null) {
-            throw e;
-        }
+      }
+      if (object == null) {
+        throw e;
+      }
     }
+
+    Map<String, Method> methodCache = classes.get(classOfT);
+    if (methodCache == null) {
+      methodCache = mapClassMethodsToJsonNames(classOfT);
+      classes.put(classOfT, methodCache);
+    }
+
+    Iterator<String> keyIterator = jsonObject.keys();
+    while (keyIterator.hasNext()) {
+      final String name = keyIterator.next();
+      Method method = methodCache.get(name);
+      if (method == null || jsonObject.isNull(name)) {
+        continue;
+      }
+
+      Class<?> parameterType = method.getParameterTypes()[0];
+      if (parameterType.isPrimitive()) {
+        if (parameterType.isAssignableFrom(boolean.class)) {
+          method.invoke(object, jsonObject.getBoolean(name));
+        } else if (parameterType.isAssignableFrom(int.class)) {
+          method.invoke(object, jsonObject.getInt(name));
+        } else if (parameterType.isAssignableFrom(long.class)) {
+          method.invoke(object, jsonObject.getLong(name));
+        } else if (parameterType.isAssignableFrom(double.class)) {
+          method.invoke(object, jsonObject.getDouble(name));
+        }
+      } else if (parameterType.isAssignableFrom(String.class)) {
+        method.invoke(object, jsonObject.getString(name));
+      } else if (parameterType.isAssignableFrom(Double.class)) {
+        method.invoke(object, jsonObject.getDouble(name));
+      } else if (parameterType.isAssignableFrom(Integer.class)) {
+        method.invoke(object, jsonObject.getInt(name));
+      } else if (parameterType.isAssignableFrom(Long.class)) {
+        method.invoke(object, jsonObject.getLong(name));
+      } else if (parameterType.isAssignableFrom(Boolean.class)) {
+        method.invoke(object, jsonObject.getBoolean(name));
+      } else if (parameterType.isArray()) {
+        method.invoke(object, parse(object, jsonObject.getJSONArray(name), parameterType));
+      } else {
+        method.invoke(object, parse(object, jsonObject.getJSONObject(name), parameterType));
+      }
+    }
+    return object;
+  }
+
+  private static Map<String, Method> mapClassMethodsToJsonNames(Class<?> classOfT) {
+    final Map<String, Method> methodCache = new HashMap<>();
     Method[] methods = classOfT.getMethods();
     for (Method method : methods) {
       String methodName = method.getName();
       Class<?>[] paramTypes = method.getParameterTypes();
       if (paramTypes != null && paramTypes.length == 1 && methodName.length() > 3 && methodName.startsWith("set")) {
         final String originalName = Character.toLowerCase(methodName.charAt(3)) + methodName.substring(4);
-        String name = null;
-        // look for the field name in the json based on the method name
-        if (jsonObject.has(originalName)) {
-          name = originalName;
-        } else if (!jsonObject.has(name = originalName.toLowerCase())) {
-          // not found as-is or lowercased? try replacing camel case with underscore
-            /*
-             * originally done using regex, but totalcross implementation has some bugs and
-             * until they are fixed this is done looping through the characters
-             * originalName.replaceAll("(.)(\\p{Upper})", "$1_$2").toLowerCase();
-             */
-          StringBuilder sb = new StringBuilder();
-          boolean lastWasUnderscored = false;
-          for(int i = 0 ; i < originalName.length() ; i++) {
-              char c = originalName.charAt(i);
-              if (!lastWasUnderscored && Character.isUpperCase(c)) {
-                  lastWasUnderscored = true;
-                  sb.append('_');
-              } else {
-                  lastWasUnderscored = false;
-              }
-              sb.append(Character.toLowerCase(c));
-          }
-          final String underscoredName = sb.toString();
-          if (jsonObject.has(underscoredName)) {
-            name = underscoredName;
-          }
-        }
-        if (!jsonObject.isNull(name)) {
-          Class<?> parameterType = method.getParameterTypes()[0];
-          if (parameterType.isPrimitive()) {
-            if (parameterType.isAssignableFrom(boolean.class)) {
-              method.invoke(object, jsonObject.getBoolean(name));
-            } else if (parameterType.isAssignableFrom(int.class)) {
-              method.invoke(object, jsonObject.getInt(name));
-            } else if (parameterType.isAssignableFrom(long.class)) {
-              method.invoke(object, jsonObject.getLong(name));
-            } else if (parameterType.isAssignableFrom(double.class)) {
-              method.invoke(object, jsonObject.getDouble(name));
-            }
-          } else if (parameterType.isAssignableFrom(String.class)) {
-            method.invoke(object, jsonObject.getString(name));
-          } else if (parameterType.isAssignableFrom(Double.class)) {
-            method.invoke(object, jsonObject.getDouble(name));
-          } else if (parameterType.isAssignableFrom(Integer.class)) {
-            method.invoke(object, jsonObject.getInt(name));
-          } else if (parameterType.isAssignableFrom(Long.class)) {
-            method.invoke(object, jsonObject.getLong(name));
-          } else if (parameterType.isAssignableFrom(Boolean.class)) {
-            method.invoke(object, jsonObject.getBoolean(name));
-          } else if (parameterType.isArray()) {
-            method.invoke(object, parse(object, jsonObject.getJSONArray(name), parameterType));
+        // name as-is
+        methodCache.put(originalName, method);
+        // lowercased name
+        methodCache.put(originalName.toLowerCase(), method);
+        // not found as-is or lowercased? try replacing camel case with underscore
+        /*
+         * originally done using regex, but totalcross implementation has some bugs and
+         * until they are fixed this is done looping through the characters
+         * originalName.replaceAll("(.)(\\p{Upper})", "$1_$2").toLowerCase();
+         */
+        StringBuilder sb = new StringBuilder();
+        boolean lastWasUnderscored = false;
+        for (int i = 0; i < originalName.length(); i++) {
+          char c = originalName.charAt(i);
+          if (!lastWasUnderscored && Character.isUpperCase(c)) {
+            lastWasUnderscored = true;
+            sb.append('_');
           } else {
-            method.invoke(object, parse(object, jsonObject.getJSONObject(name), parameterType));
+            lastWasUnderscored = false;
           }
+          sb.append(Character.toLowerCase(c));
         }
+        final String underscoredName = sb.toString();
+        methodCache.put(underscoredName, method);
       }
     }
-    return object;
+    return methodCache;
   }
 }
