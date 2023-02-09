c83201e02020b361f700d4c0b2e3922806660147
Fabio Sobral <fabio@totalcross.com>
2023-02-09 19:26:08 -0300

    build(script,ios): warn about mbedtls dependencies in xcode
    Also copy the files back into the build output.
    
    (cherry picked from commit 2ec666c6d00593d809d9843d6ab00fec49a2041f)

--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+BASEDIR=$(cd ..; pwd)
+OUTDIR=$BASEDIR/build/TotalCross
+
+echo "STARTING iOS"
+
 mkdir ~/build
 cd ~/build
-
-BASEDIR=${PWD}
 
 rm -rf ios*
 
@@ -12,10 +15,15 @@ git clone --shallow-submodules --depth 1 --single-branch --branch master git@git
 pushd ios/TotalCrossVM/xcode
    cmake ../ -GXcode
    pod install
+   # Make sure legacy build system is enabled
+   /usr/libexec/PlistBuddy -c "Add :DisableBuildSystemDeprecationDiagnostic bool" TotalCross.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
+   /usr/libexec/PlistBuddy -c "Set :DisableBuildSystemDeprecationDiagnostic true" TotalCross.xcworkspace/xcshareddata/WorkspaceSettings.xcsettings
+   read -p "Fix the mbedTLS dependencies and press any key to continue... " -n1 -s
    xcodebuild -workspace TotalCross.xcworkspace -scheme TotalCross BUILD_DIR=${PWD} archive -archivePath build/TotalCross.xcarchive teamID="W5Y7X2KNUL" -configuration Release
    xcodebuild -exportArchive -archivePath build/TotalCross.xcarchive -exportPath build/TotalCross.ipa -exportOptionsPlist ExportOptions.plist
 popd
 
 pushd ios/TotalCrossVM/builders/xcode/build
-   zip -r -q $BASEDIR/ios.zip .
+   cp TotalCross.ipa/TotalCross.ipa $OUTDIR/dist/vm/ios/
+   cp -R TotalCross.xcarchive $OUTDIR/etc/tools/iOSCodesign/
 popd
