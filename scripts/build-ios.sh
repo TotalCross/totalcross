43ccdf3f64a8cf164ddffd40c4505448582323a4
Fabio Sobral <flsobral@gmail.com>
2023-10-09 16:45:38 -0300

    build(iOS): moves ios build files to root project for use with cmake
    (cherry picked from commit e2ce1d411333e02440adce6e9281d04d418b307c)

--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir ~/build
 cd ~/build
 
 BASEDIR=${PWD}
@@ -7,9 +8,9 @@ BASEDIR=${PWD}
 rm -rf ios*
 
 git clone --shallow-submodules --depth 1 --single-branch --branch master git@gitlab.com:totalcross/vm.git ios
-unzip ScanditBarcodeScanner.framework-b1.zip -d ios/TotalCrossVM/builders/xcode
 
-pushd ios/TotalCrossVM/builders/xcode
+pushd ios/TotalCrossVM/xcode
+   cmake ../ -GXcode
    pod install
    xcodebuild -workspace TotalCross.xcworkspace -scheme TotalCross BUILD_DIR=${PWD} archive -archivePath build/TotalCross.xcarchive teamID="W5Y7X2KNUL" -configuration Release
    xcodebuild -exportArchive -archivePath build/TotalCross.xcarchive -exportPath build/TotalCross.ipa -exportOptionsPlist ExportOptions.plist
