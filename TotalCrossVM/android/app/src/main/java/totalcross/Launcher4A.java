6a729e87851a893009e1380cd630ee72e0157672
Fabio Sobral <flsobral@gmail.com>
2024-02-08 15:24:56 -0300

    fix(permissions,android): use media permissions on android 13 and above
    Use READ_MEDIA_IMAGES and READ_MEDIA_VIDEOS instead of the older
    external storage permissions.
    
    (cherry picked from commit 484bde839d527865f6699165ab20a78c14511a79)

--- TotalCrossVM/android/app/src/main/java/totalcross/Launcher4A.java
+++ TotalCrossVM/android/app/src/main/java/totalcross/Launcher4A.java
@@ -1803,8 +1803,8 @@ final public class Launcher4A extends SurfaceView implements SurfaceHolder.Callb
     public static final PermissionHandler CAMERA = new PermissionHandler(
             Loader.PermissionRequestCodes.CAMERA,
             Manifest.permission.CAMERA,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU ? Manifest.permission.READ_MEDIA_IMAGES : Manifest.permission.READ_EXTERNAL_STORAGE,
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU ? Manifest.permission.READ_MEDIA_VIDEO : Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
     public static final PermissionHandler BLUETOOTH = new PermissionHandler(
             Loader.PermissionRequestCodes.BLUETOOTH,
