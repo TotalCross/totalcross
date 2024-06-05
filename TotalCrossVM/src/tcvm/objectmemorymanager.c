92a507f32522911482d1d8332dbadc58736d31b0
Fabio Sobral <flsobral@gmail.com>
2024-06-05 13:42:32 -0300

    perf(vm,macos): increase default memory allocation chunk size
    macOS was using the same default value as WinCE, which caused
    performance issues for memory-intensive operations such as JSONTokener.
    
    (cherry picked from commit 0bfcdd883db1d7a09995b456678cf9f8aa86aa54)

--- TotalCrossVM/src/tcvm/objectmemorymanager.c
+++ TotalCrossVM/src/tcvm/objectmemorymanager.c
@@ -145,7 +145,7 @@ pointer to next):
 void gc2(Context currentContext, bool lockOMM);
 void soundTone(int32 frequency, int32 duration);
 
-#if (defined(WIN32) && !defined(WINCE)) || defined(darwin) || defined(ANDROID)
+#if (defined(WIN32) && !defined(WINCE)) || defined(darwin) || defined(ANDROID) || defined (__APPLE__)
 #define DEFAULT_CHUNK_SIZE (1024*1024-48)
 #else
 #define DEFAULT_CHUNK_SIZE (64*1024-64) 
