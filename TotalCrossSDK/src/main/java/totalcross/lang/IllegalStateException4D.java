844ee3338b222c9cbb20a3e1b80431a62874e625
Fabio Sobral <flsobral@gmail.com>
2026-07-22 19:40:16 -0300

    fix(sdk): add illegal state cause constructors

--- TotalCrossSDK/src/main/java/totalcross/lang/IllegalStateException4D.java
+++ TotalCrossSDK/src/main/java/totalcross/lang/IllegalStateException4D.java
@@ -1,32 +1,26 @@
 // Copyright (C) 2000-2013 SuperWaba Ltda.
-// Copyright (C) 2014-2020 TotalCross Global Mobile Platform Ltda.
+// Copyright (C) 2014-2021 TotalCross Global Mobile Platform Ltda.
+// Copyright (C) 2022-2026 Amalgam Solucoes em TI Ltda
 //
 // SPDX-License-Identifier: LGPL-2.1-only
 
 package totalcross.lang;
 
-/**
- * Signals that a method has been invoked at an illegal or inappropriate time. <br>
- * <br>
- * IMPORTANT: the totalcross.lang package is the java.lang that will be used in the device. You CANNOT use nor import
- * totalcross.lang package in desktop. When tc.Deploy is called, all references to java.lang are replaced by
- * totalcross.lang automatically. Given this, you must use only the classes and methods that exists BOTH in java.lang
- * and totalcross.lang. For example, you can't use java.lang.ClassLoader because there are no
- * totalcross.lang.ClassLoader. Another example, you can't use java.lang.String.indexOfIgnoreCase because there are no
- * totalcross.lang.String.indexOfIgnoreCase method. Trying to use a class or method from the java.lang package that has
- * no correspondence with totalcross.lang will make the tc.Deploy program to abort, informing where the problem occured.
- * A good idea is to always refer to this javadoc to know what is and what isn't available.
- */
-
 public class IllegalStateException4D extends RuntimeException {
   static final long serialVersionUID = -1848914673093119416L;
 
-  /** Constructs an empty Exception. */
   public IllegalStateException4D() {
   }
 
-  /** Constructs an exception with the given message. */
   public IllegalStateException4D(String message) {
     super(message);
+  }
+
+  public IllegalStateException4D(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public IllegalStateException4D(Throwable cause) {
+    super(cause);
   }
 }
