c879bad09686b54db6305d59a6e3b63c64c6265e
Fabio Sobral <flsobral@gmail.com>
2026-08-01 15:57:45 -0300

    fix(vm): keep Skia metrics logical

--- TotalCrossVM/src/nm/ui/PalmFont_c.h
+++ TotalCrossVM/src/nm/ui/PalmFont_c.h
@@ -721,12 +721,12 @@ UserFont loadUserFontFromFontObj(Context currentContext, TCObject fontObj, JChar
 #include "skia/skia.h"
 
 int32 getJCharWidth(Context currentContext, TCObject fontObj, JChar ch) {
-  int32 fontSize = (int)(Font_size(fontObj) * (*tcSettings.screenDensityPtr));
+  int32 fontSize = Font_size(fontObj);
   return skia_stringWidth(&ch, sizeof(JChar), Font_skiaIndex(fontObj), fontSize);
 }
 
 int32 getJCharPWidth(Context currentContext, TCObject fontObj, JCharP s, int32 len) {
-   int32 fontSize = (int)(Font_size(fontObj) * (*tcSettings.screenDensityPtr));
+   int32 fontSize = Font_size(fontObj);
     return len == 0? 0: skia_stringWidth(s, len * sizeof(JChar), Font_skiaIndex(fontObj), fontSize);
 }
 #else
