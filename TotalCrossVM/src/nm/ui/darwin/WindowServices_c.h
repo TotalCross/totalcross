ffdab187fe0edd796d08b023a87f49476bccb3ed
Fabio Sobral <flsobral@gmail.com>
2026-09-01 13:08:33 -0300

    fix(headers): align new window service copyrights
    Apply the repository's new-file copyright policy to the Android and iOS service adapters while retaining their LGPL-2.1-only declarations.
    
    Validate both touched headers before commit.

--- TotalCrossVM/src/nm/ui/darwin/WindowServices_c.h
+++ TotalCrossVM/src/nm/ui/darwin/WindowServices_c.h
@@ -1,6 +1,4 @@
-// Copyright (C) 2000-2013 SuperWaba Ltda.
-// Copyright (C) 2014-2021 TotalCross Global Mobile Platform Ltda.
-// Copyright (C) 2022-2026 Amalgam Solucoes em TI Ltda
+// Copyright (C) 2026 Amalgam Solucoes em TI Ltda
 //
 // SPDX-License-Identifier: LGPL-2.1-only
 
